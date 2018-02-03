7e84f4de9d7da4b396e84687a5e2942ee2a5c759
WTer <wesleyliu@qq.com>
2018-02-03 19:45:40 +0800

    Add and refine unittests for VPacket (#10)
    * Refine coding style
    
    1. Replace tab with 4 spaces;
    2. Remove some the dead and obsolete code;
    3. Remove Chinese comments;
    4. Refine comments style to avoid golint warnings.
    
    * Add and refine unittests for VPacket
    
    * Addressed comments.
    
    * Change the type of VPacketHeadLen and dataLen from uint16 to uint

--- p2p/vpacket.go
+++ p2p/vpacket.go
@@ -1,17 +1,19 @@
 /**
-*  @file
+*  Package p2p
 *  @copyright defined in go-seele/LICENSE
  */
 package p2p
 
-import "encoding/binary"
+import  (
+    "encoding/binary"
+)
 
 const (
-    //VPacketHeadLen head length in net-package
+    // VPacketHeadLen head length in net-package
     VPacketHeadLen uint = 13
 )
 
-//VPacket package for vqic
+// VPacket package for QVIC
 type VPacket struct {
     seq                uint32
     packType           byte
@@ -22,8 +24,8 @@ type VPacket struct {
     createTick         uint16
     data               []byte
     dataLen            uint
-    isRecovered        int //0 for not ok; 1 for ok. for client: 1, recved ack; for server 1 ack sent;
-    isSendedToTun      int //only avalible for recver. 0 no; 1 , already write to tun
+    isRecovered        int // 1: ok; 0: others. for client, 1: recved ack; for server, 1: ack sent.
+    isSendedToTun      int // only avalible for recovering. 0: no; 1: already write to tun
     isSendedToPeer     bool
     sendTimes          int
     lastSendTickLocal  uint16
@@ -32,7 +34,7 @@ type VPacket struct {
     dataNet            [1500]byte
 }
 
-//MarshalData pack data to dataNet
+// MarshalData pack data to dataNet
 func (v *VPacket) MarshalData() {
     b := v.dataNet[0:]
     binary.BigEndian.PutUint32(b[:4], v.seq)
@@ -44,7 +46,7 @@ func (v *VPacket) MarshalData() {
     copy(b[VPacketHeadLen:], v.data)
 }
 
-//ParseData parse data from dataNet. packData contains udp-package recved from net
+// ParseData parse data from dataNet. packData contains udp-package recved from net
 func (v *VPacket) ParseData(packData []byte) {
     b := v.dataNet[0:]
     copy(b, packData)
@@ -54,26 +56,25 @@ func (v *VPacket) ParseData(packData []byte) {
     v.magic = binary.BigEndian.Uint16(b[7:9])
     v.lastSeqSendTick = binary.BigEndian.Uint16(b[9:11])
     v.createTick = binary.BigEndian.Uint16(b[11:13])
-
     v.dataLen = uint(len(packData)) - VPacketHeadLen
     v.data = make([]byte, v.dataLen)
     copy(v.data, b[VPacketHeadLen:])
 }
 
-//FECInfo record packages info of a bundle
+// FECInfo record packages info of a bundle
 type FECInfo struct {
     seq        uint32
     fecPackets [8]*VPacket
     fecFlag    byte
 }
 
-//NewFECInfo create new FECInfo class
+// NewFECInfo create new FECInfo class
 func NewFECInfo() (f *FECInfo) {
     f = new(FECInfo)
     return f
 }
 
-//Clear reset FECInfo
+// Clear reset FECInfo
 func (v *FECInfo) Clear() {
     if v.fecFlag == 0 {
         return
@@ -86,7 +87,7 @@ func (v *FECInfo) Clear() {
     v.fecFlag = 0
 }
 
-//VPacketItem package info for RecverMgr
+// VPacketItem package info for RecverMgr
 type VPacketItem struct {
     pFECInfo      *FECInfo
     p             *VPacket
--- p2p/vpacket_test.go
+++ p2p/vpacket_test.go
@@ -5,21 +5,75 @@
 package p2p
 
 import (
+    "bytes"
     "fmt"
     "testing"
 )
 
-func Test_VTunPacket_1(t *testing.T) {
+func Test_MarshalData(t *testing.T) {
+    v1 := createVPacket("Golang");
+    v1.MarshalData()
+
+    // meta info
+    if v1.seq != 101 || v1.packType != 2 || v1.fecIdx != 4 || v1.crc != 110 || v1.magic != 120 || 
+       v1.lastSeqSendTick != 0 || v1.createTick != 0 || v1.dataLen != 6 {
+        fmt.Println("Test_MarshalData:", v1.seq, v1.packType, v1.fecIdx, v1.crc, v1.magic, v1.lastSeqSendTick, v1.createTick, v1.dataLen)
+        t.Fail();
+    }
+
+    // data
+    if !bytes.Equal(v1.data, []byte("Golang")) {
+        fmt.Println("Test_MarshalData data:", v1.data)
+        t.Fail();
+    }
+
+    // data is included in dataNet
+    if !bytes.Equal(v1.dataNet[13:19], v1.data) {
+        fmt.Println("Test_MarshalData dataNet:", v1.dataNet[13:19], v1.data)
+        t.Fail();
+    }
+
+    // rest of dataNet must be 0
+    zeroArray := [1500]byte{}
+    if !bytes.Equal(v1.dataNet[19:], zeroArray[19:]) {
+        fmt.Println("Test_MarshalData:", v1.dataNet[19:])
+        t.Fail();
+    }
+}
+
+func Test_ParseData(t *testing.T) {
+    v1 := createVPacket("Golang");
+    v1.MarshalData();
+
+    v2 := createVPacket("should be the same as Golang");
+    v2.ParseData(v1.dataNet[:])
+
+    // meta info
+    if v1.seq != v2.seq || v1.packType != v2.packType || v1.fecIdx != v2.fecIdx || v1.crc != v2.crc || v1.magic != v2.magic || 
+       v1.lastSeqSendTick != v2.lastSeqSendTick || v1.createTick != v2.createTick {
+        fmt.Println("Test_ParseData v1:", v1.seq, v1.packType, v1.fecIdx, v1.crc, v1.magic, v1.lastSeqSendTick, v1.createTick)
+        fmt.Println("Test_ParseData v2:", v2.seq, v2.packType, v2.fecIdx, v2.crc, v2.magic, v2.lastSeqSendTick, v2.createTick)
+        t.Fail();
+    }
+
+    // data
+    if !bytes.Equal(v1.data[:6], v2.data[:6]) {
+        fmt.Println("Test_ParseData data:", v1.data, v2.data)
+        t.Fail();
+    }
+
+    // dataNet
+    if !bytes.Equal(v1.dataNet[:], v2.dataNet[:]) {
+        fmt.Println("Test_ParseData dataNet:", v1.dataNet, v2.dataNet)
+        t.Fail();
+    }
+}
+
+func createVPacket(data string) (v *VPacket) {
     v1 := new(VPacket)
     v1.seq, v1.packType, v1.fecIdx, v1.crc, v1.magic = 101, 2, 4, 110, 120
-    v1.data = []byte("Golang")
+    v1.data = []byte(data)
     v1.dataLen = uint(len(v1.data))
 
-    v1.MarshalData()
-    v2 := new(VPacket)
-    slice1 := v1.dataNet[:v1.dataLen+VPacketHeadLen]
-
-    v2.ParseData(slice1)
-    fmt.Println("Test_VTunPacket_1 1", v1.seq, v1.packType, v1.fecIdx, v1.crc, v1.magic, v1.data, v1.dataLen)
-    fmt.Println("Test_VTunPacket_1 2", v2.seq, v2.packType, v2.fecIdx, v2.crc, v2.magic, v2.data, v2.dataLen)
+    return v1;
 }
