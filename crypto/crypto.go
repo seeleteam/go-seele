1b52b07c83114dd6781ae09354ed4d9588e1a2d7
lucus <nikehao_love@163.com>
2018-02-06 17:10:21 +0800

    Add common and crypto function (#18)
    * add commons
    
    * rename file
    
    * revert
    
    * add test
    
    * add vendor

---
+++ crypto/crypto.go
@@ -0,0 +1,37 @@
+/**
+*  @file
+*  @copyright defined in go-seele/LICENSE
+ */
+package crypto
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+
+	"github.com/seeleteam/go-seele/crypto/secp256k1"
+)
+
+func GenerateKey() (*ecdsa.PrivateKey, error) {
+	return ecdsa.GenerateKey(S256(), rand.Reader)
+}
+
+func ToECDSAPub(pub []byte) *ecdsa.PublicKey {
+	if len(pub) == 0 {
+		return nil
+	}
+	x, y := elliptic.Unmarshal(S256(), pub)
+	return &ecdsa.PublicKey{Curve: S256(), X: x, Y: y}
+}
+
+func FromECDSAPub(pub *ecdsa.PublicKey) []byte {
+	if pub == nil || pub.X == nil || pub.Y == nil {
+		return nil
+	}
+	return elliptic.Marshal(pub.Curve, pub.X, pub.Y)
+}
+
+// S256 returns an instance of the secp256k1 curve.
+func S256() elliptic.Curve {
+	return secp256k1.S256()
+}
