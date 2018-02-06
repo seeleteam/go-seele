24914f3d740205c52a3e80c2880d777c264f6941
lucus <nikehao_love@163.com>
2018-02-06 16:58:29 +0800

    add test

--- common/hexutil.go
+++ common/hexutil.go
@@ -20,16 +20,16 @@ type decError struct{ msg string }
 
 func (err decError) Error() string { return err.msg }
 
-// Encode encodes b as a hex string with 0x prefix.
-func Encode(b []byte) string {
+// BytesToHex encodes b as a hex string with 0x prefix.
+func BytesToHex(b []byte) string {
 	enc := make([]byte, len(b)*2+2)
 	copy(enc, "0x")
 	hex.Encode(enc[2:], b)
 	return string(enc)
 }
 
-// Decode decodes a hex string with 0x prefix.
-func Decode(input string) ([]byte, error) {
+// HexToBytes decodes a hex string with 0x prefix.
+func HexToBytes(input string) ([]byte, error) {
 	if len(input) == 0 {
 		return nil, ErrEmptyString
 	}
---
+++ common/hexutil_test.go
@@ -0,0 +1,28 @@
+/**
+*  @file
+*  @copyright defined in go-seele/LICENSE
+ */
+package common
+
+import (
+	"testing"
+
+	"github.com/magiconair/properties/assert"
+	"github.com/seeleteam/go-seele/log"
+)
+
+func Test_Hex(t *testing.T) {
+	str := "0x5aaeb6053f3e94c9b9a09f33669435e7ef1beaed"
+
+	bytes, err := HexToBytes(str)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res := BytesToHex(bytes)
+
+	assert.Equal(t, res, str)
+
+	bytes, err = HexToBytes("")
+	assert.Equal(t, err, ErrEmptyString)
+}
