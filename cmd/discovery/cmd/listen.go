19aafa45cf955ef11fa438319c5c66f8bd8812bc
lucus <nikehao_love@163.com>
2018-02-06 19:57:46 +0800

    add command line tool for discovery (#20)
    * add lincense and resolve warning
    
    * add folder
    
    * commit for sync
    
    * add vendor
    
    * night 1
    
    * complete ping pong
    
    * fmt
    
    * revert
    
    * set lincense

---
+++ cmd/discovery/cmd/listen.go
@@ -0,0 +1,44 @@
+/**
+*  @file
+*  @copyright defined in go-seele/LICENSE
+ */
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/seeleteam/go-seele/p2p/discovery"
+	"github.com/spf13/cobra"
+)
+
+// listenCmd represents the listen command
+var listenCmd = &cobra.Command{
+	Use:   "listen",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("listen called")
+
+		discovery.StartServer("9000")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listenCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// listenCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// listenCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ cmd/discovery/cmd/send.go
@@ -0,0 +1,45 @@
+/**
+*  @file
+*  @copyright defined in go-seele/LICENSE
+ */
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/seeleteam/go-seele/p2p/discovery"
+
+	"github.com/spf13/cobra"
+)
+
+// sendCmd represents the send command
+var sendCmd = &cobra.Command{
+	Use:   "send",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("send called")
+
+		discovery.SendPing("9001", "9000")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(sendCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// sendCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// sendCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
