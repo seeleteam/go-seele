6c9a979f92e0287462af7dcc49d1ca860485ef8f
qiubo <82121246@qq.com>
2018-02-12 17:27:25 +0800

    init contract - reg/invoke

---
+++ contract/handler.go
@@ -0,0 +1,50 @@
+package contract
+
+import (
+	"github.com/seeleteam/go-seele/crypto"
+	"github.com/seeleteam/go-seele/log"
+)
+
+// HandleTransaction handles smart contract transation.
+func HandleTransaction(tx *Transaction) {
+	hash := crypto.Keccak256Hash(tx.payload)
+	if !tx.sig.Verify(hash) {
+		log.Error("Transaction signature is invalid, and will not be archived into blockchain.")
+		return
+	}
+
+	switch tx.txType {
+	case txTypeContractReg:
+		onRegisterContract(tx)
+	case txTypeContractCall:
+		onInvokeContract(tx)
+	case txTypeContractDel:
+		onDeleteContrct(tx)
+	}
+}
+
+func onRegisterContract(tx *Transaction) {
+	codeAddr := codeAddress(tx.payload)
+	ChainServ.WriteCode(codeAddr, tx.payload)
+
+	// TODO run smart contract for init?
+	account := &Account{
+		AccountAddress: tx.from,
+		CodeAddress:    codeAddr,
+		State:          make([]byte, 0),
+	}
+	ChainServ.WriteContractAccount(account)
+
+	ChainServ.WriteTransaction(tx)
+}
+
+func onInvokeContract(tx *Transaction) {
+	account := ChainServ.GetContractAccount(tx.to)
+	code := ChainServ.GetCode(account.CodeAddress)
+	vm := exeVM{}
+	vm.Execute(code, tx.payload)
+}
+
+func onDeleteContrct(tx *Transaction) {
+	// TODO logic vs. physical deletion.
+}
