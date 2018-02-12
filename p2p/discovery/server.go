db9169b3ca34d5cbe543fd51692b7d86eb5b30b8
lucus <nikehao_love@163.com>
2018-02-12 10:34:58 +0800

    Change discovery cmd line for product (#32)
    * change cmd for product
    
    * fix
    
    * format
    
    * add usage
    
    * add license

--- p2p/discovery/server.go
+++ p2p/discovery/server.go
@@ -10,48 +10,14 @@ import (
 	"sync"
 
 	"github.com/seeleteam/go-seele/common"
-	"github.com/seeleteam/go-seele/common/hexutil"
-	"github.com/seeleteam/go-seele/log"
 )
 
-func StartService(id common.Address, addr *net.UDPAddr) {
-	udp := newUDP(id, addr)
-
-	udp.StartServe()
-
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-	wg.Wait()
-}
-
-func getUDP(port string, id common.Address) *udp {
-	addr, err := net.ResolveUDPAddr("udp", ":"+port)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	return newUDP(id, addr)
-}
-
-func hexToAddress(id string) common.Address {
-	byte, err := hexutil.HexToBytes(id)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	nid, err := common.NewAddress(byte)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	return nid
-}
-
-func SendPing(myId common.Address, myAddr *net.UDPAddr, id common.Address, targeAddr *net.UDPAddr) {
+func StartService(myId common.Address, myAddr *net.UDPAddr, bootstrap *Node) {
 	udp := newUDP(myId, myAddr)
 
-	n := NewNodeWithAddr(id, targeAddr)
-	udp.addNode(n)
+	if bootstrap != nil {
+		udp.addNode(bootstrap)
+	}
 
 	udp.StartServe()
 
