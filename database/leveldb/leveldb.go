45c84195ab5e5f83e17658863eece26229a69673
WTer <wesleyliu@qq.com>
2018-02-28 14:48:24 +0800

    Add some func comments

--- database/leveldb/leveldb.go
+++ database/leveldb/leveldb.go
@@ -31,36 +31,44 @@ func (db *levelDB) Close() {
 	db.db.Close()
 }
 
+// Get gets the value for the given key
 func (db *levelDB) GetString(key string) (string, error) {
 	value, err := db.Get([]byte(key))
 
 	return string(value), err
 }
 
+// Get gets the value for the given key
 func (db *levelDB) Get(key []byte) ([]byte, error) {
 	return db.db.Get(key, nil)
 }
 
+// Put sets the value for the given key
 func (db *levelDB) Put(key []byte, value []byte) error {
 	return db.db.Put(key, value, nil)
 }
 
+// Put sets the value for the given key
 func (db *levelDB) PutString(key string, value string) error {
 	return db.Put([]byte(key), []byte(value))
 }
 
+// Has returns true if the DB does contains the given key.
 func (db *levelDB) Has(key []byte) (ret bool, err error) {
 	return db.db.Has(key, nil)
 }
 
+// Has returns true if the DB does contains the given key.
 func (db *levelDB) HasString(key string) (ret bool, err error) {
 	return db.Has([]byte(key))
 }
 
+// Delete deletes the value for the given key.
 func (db *levelDB) Delete(key []byte) error {
 	return db.db.Delete(key, nil)
 }
 
+// Delete deletes the value for the given key.
 func (db *levelDB) DeleteSring(key string) error {
 	return db.Delete([]byte(key))
 }
