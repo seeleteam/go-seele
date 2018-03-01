a024068271a5c1e051a4ee0886916dd778b7b043
Leihao Zhou <herove@gmail.com>
2018-03-01 10:55:34 +0800

    add comments

--- node/service.go
+++ node/service.go
@@ -9,6 +9,7 @@ import "github.com/seeleteam/go-seele/p2p"
 
 // Service registers to node after node starts.
 type Service interface {
+	// Protocols retrieves the P2P protocols the service wishes to start.
 	Protocols() []p2p.Protocol
 
 	Start(server *p2p.Server) error
