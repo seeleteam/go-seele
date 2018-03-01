27ea56017fe2c167d7398481b88f7704a5a3ff5f
reedhong <fishwarter@qq.com>
2018-03-01 17:14:45 +0800

    add node cmd

---
+++ cmd/node/cmd/start.go
@@ -0,0 +1,33 @@
+/**
+*  @file
+*  @copyright defined in go-seele/LICENSE
+ */
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/seeleteam/go-seele/node"
+	"github.com/spf13/cobra"
+)
+
+// startCmd represents the start command
+var startCmd = &cobra.Command{
+	Use:   "start",
+	Short: "start the node of seele",
+	Long: `usage example:
+    node start 
+		start a node.`,
+
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("start called")
+
+		seeleNode := &node.Node{}
+		seeleNode.Start()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(startCmd)
+}
