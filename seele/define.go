f0be56368cbe212054c7997c5e698d065db964c0
qiaozhigang <zhigang.qiao@gmail.com>
2018-03-03 16:25:18 +0800

    Refector code

--- seele/define.go
+++ seele/define.go
@@ -6,14 +6,9 @@
 package seele
 
 const (
-<<<<<<< HEAD
 	// SeeleProtoName protoName of Seele service
 	SeeleProtoName = "seele"
 
 	// SeeleVersion Version number of Seele protocol
 	SeeleVersion uint = 1
-=======
-	SeeleProtoName      = "seele"
-	SeeleVersion   uint = 1
->>>>>>> Add seele service framework
 )
