230e8c8b9bf65efa58ffb4c4e407d266ae271625
qiaozhigang <zhigang.qiao@gmail.com>
2018-03-03 16:15:43 +0800

    Add seele service framework

---
+++ seele/define.go
@@ -0,0 +1,11 @@
+/**
+*  @file
+*  @copyright defined in go-seele/LICENSE
+ */
+
+package seele
+
+const (
+	SeeleProtoName      = "seele"
+	SeeleVersion   uint = 1
+)
---
+++ seele/seeleprotocol.go
@@ -0,0 +1,64 @@
+/**
+*  @file
+*  @copyright defined in go-seele/LICENSE
+ */
+
+package seele
+
+import (
+	"github.com/seeleteam/go-seele/log"
+	"github.com/seeleteam/go-seele/p2p"
+)
+
+// SeeleProtocol
+type SeeleProtocol struct {
+	p2p.Protocol
+	maxPeers int
+	peers    map[*p2p.Peer]bool
+	log      *log.SeeleLog
+}
+
+// NewSeeleService create SeeleProtocol
+func NewSeeleProtocol(networkID uint64, log *log.SeeleLog) (s *SeeleProtocol, err error) {
+	s = &SeeleProtocol{
+		Protocol: p2p.Protocol{
+			Name:      SeeleProtoName,
+			Version:   SeeleVersion,
+			AddPeerCh: make(chan *p2p.Peer),
+			DelPeerCh: make(chan *p2p.Peer),
+			ReadMsgCh: make(chan *p2p.Message),
+		},
+		log:   s.log,
+		peers: make(map[*p2p.Peer]bool),
+	}
+	return s, nil
+}
+
+// Run implements p2p.Protocol, called in p2p.Server.Start function
+func (p *SeeleProtocol) Run() {
+	p.log.Info("SeeleProtocol started...")
+	for {
+		select {
+		case peer := <-p.AddPeerCh:
+			p.peers[peer] = true
+		case peer := <-p.DelPeerCh:
+			delete(p.peers, peer)
+		case message := <-p.ReadMsgCh:
+			p.log.Info("SeeleProtocol readmsg [%s]", message)
+		}
+	}
+}
+
+// GetBaseProtocol implements p2p.Protocol
+func (p SeeleProtocol) GetBaseProtocol() (baseProto *p2p.Protocol) {
+	return &(p.Protocol)
+}
+
+func (p *SeeleProtocol) handleMsg(msg *p2p.Message) error {
+	return nil
+}
+
+// Stop stop protocol, called when seeleService quits.
+func (p SeeleProtocol) Stop() {
+	//TODO add a quit channel
+}
