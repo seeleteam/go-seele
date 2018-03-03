7ac7db22187b15366b021768ae32ac3c1d3c598b
qiaozhigang <zhigang.qiao@gmail.com>
2018-03-03 16:15:43 +0800

    Add seele service framework

--- seele/define.go
+++ seele/define.go
@@ -6,9 +6,14 @@
 package seele
 
 const (
+<<<<<<< HEAD
 	// SeeleProtoName protoName of Seele service
 	SeeleProtoName = "seele"
 
 	// SeeleVersion Version number of Seele protocol
 	SeeleVersion uint = 1
+=======
+	SeeleProtoName      = "seele"
+	SeeleVersion   uint = 1
+>>>>>>> Add seele service framework
 )
