fa351e164a15e8da6e0fe4467459213a6d5aedf5
lucus <nikehao_love@163.com>
2018-03-04 15:56:39 +0800

    add merkle tree (#46)
    * add merkle tree
    
    * resolve comments
    
    * fix test
    
    * rename test
    
    * resolve comments and refine hash method
    
    * rename method

--- database/leveldb/leveldb.go
+++ database/leveldb/leveldb.go
@@ -7,19 +7,25 @@ package leveldb
 
 import (
 	"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb/errors"
 )
 
-type levelDB struct {
+type LevelDB struct {
 	db *leveldb.DB
 }
 
-func NewLevelDB(path string) (*levelDB, error) {
+func NewLevelDB(path string) (*LevelDB, error) {
 	db, err := leveldb.OpenFile(path, nil)
+
+	if _, corrupted := err.(*errors.ErrCorrupted); corrupted {
+		db, err = leveldb.RecoverFile(path, nil)
+	}
+
 	if err != nil {
 		return nil, err
 	}
 
-	result := &levelDB{
+	result := &LevelDB{
 		db: db,
 	}
 
@@ -27,48 +33,48 @@ func NewLevelDB(path string) (*levelDB, error) {
 }
 
 // Close don't forget close db when not use
-func (db *levelDB) Close() {
+func (db *LevelDB) Close() {
 	db.db.Close()
 }
 
 // Get gets the value for the given key
-func (db *levelDB) GetString(key string) (string, error) {
+func (db *LevelDB) GetString(key string) (string, error) {
 	value, err := db.Get([]byte(key))
 
 	return string(value), err
 }
 
 // Get gets the value for the given key
-func (db *levelDB) Get(key []byte) ([]byte, error) {
+func (db *LevelDB) Get(key []byte) ([]byte, error) {
 	return db.db.Get(key, nil)
 }
 
 // Put sets the value for the given key
-func (db *levelDB) Put(key []byte, value []byte) error {
+func (db *LevelDB) Put(key []byte, value []byte) error {
 	return db.db.Put(key, value, nil)
 }
 
 // Put sets the value for the given key
-func (db *levelDB) PutString(key string, value string) error {
+func (db *LevelDB) PutString(key string, value string) error {
 	return db.Put([]byte(key), []byte(value))
 }
 
 // Has returns true if the DB does contains the given key.
-func (db *levelDB) Has(key []byte) (ret bool, err error) {
+func (db *LevelDB) Has(key []byte) (ret bool, err error) {
 	return db.db.Has(key, nil)
 }
 
 // Has returns true if the DB does contains the given key.
-func (db *levelDB) HasString(key string) (ret bool, err error) {
+func (db *LevelDB) HasString(key string) (ret bool, err error) {
 	return db.Has([]byte(key))
 }
 
 // Delete deletes the value for the given key.
-func (db *levelDB) Delete(key []byte) error {
+func (db *LevelDB) Delete(key []byte) error {
 	return db.db.Delete(key, nil)
 }
 
 // Delete deletes the value for the given key.
-func (db *levelDB) DeleteSring(key string) error {
+func (db *LevelDB) DeleteSring(key string) error {
 	return db.Delete([]byte(key))
 }
