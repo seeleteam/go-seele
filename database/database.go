55556b92c60ce2a45a2280edca8eb43ce9db918f
jessonzheng <35758558+jessonzheng@users.noreply.github.com>
2018-03-05 18:34:10 +0800

    add batch&database interface;add batch for leveldb implement (#55)
    * add caller for log module
    
    * add comment;use const var for levels;format code
    
    * change comment
    
    * Merge branch 'master' into dev
    
    * Merge branch 'master' into dev
    
    * add batch&database interface;add leveldb batch impl
    
    * Merge remote-tracking branch 'origin/dev' into dev
    
    * remove Database&Close interface of Batch; fix format
    
    * remove database in batch struct

---
+++ database/database.go
@@ -0,0 +1,28 @@
+/**
+*  @file
+*  @copyright defined in go-seele/LICENSE
+ */
+
+package database
+
+// Database interface of store
+type Database interface {
+	Close()
+	Put(key []byte, value []byte) error
+	Get(key []byte) ([]byte, error)
+	GetString(key string) (string, error)
+	PutString(key string, value string) error
+	Has(key []byte) (ret bool, err error)
+	HasString(key string) (ret bool, err error)
+	Delete(key []byte) error
+	DeleteSring(key string) error
+	NewBatch() Batch
+}
+
+// Batch interface of batch for database
+type Batch interface {
+	Put(key []byte, value []byte)
+	Delete(key []byte)
+	Commit() error
+	Rollback()
+}
