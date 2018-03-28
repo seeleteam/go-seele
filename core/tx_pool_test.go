3f62c2b5b3cff07cddcfabf36d3f6c717ea959a5
lucus <nikehao_love@163.com>
2018-03-28 13:03:28 +0800

    fix test failed

--- core/tx_pool_test.go
+++ core/tx_pool_test.go
@@ -41,9 +41,8 @@ func Test_TransactionPool_Add_ValidTx(t *testing.T) {
 	pool := NewTransactionPool(*DefaultTxPoolConfig())
 	tx := newTestTx(t, 10, 100)
 
-	added, err := pool.AddTransaction(tx)
+	err := pool.AddTransaction(tx)
 
-	assert.Equal(t, added, true)
 	assert.Equal(t, err, error(nil))
 	assert.Equal(t, len(pool.hashToTxMap), 1)
 }
@@ -54,9 +53,8 @@ func Test_TransactionPool_Add_InvalidTx(t *testing.T) {
 
 	// Change the amount in tx.
 	tx.Data.Amount.SetInt64(20)
-	added, err := pool.AddTransaction(tx)
+	err := pool.AddTransaction(tx)
 
-	assert.Equal(t, added, false)
 	if err == nil {
 		t.Fatal("The error is nil when add invalid tx to pool.")
 	}
@@ -66,12 +64,10 @@ func Test_TransactionPool_Add_DuplicateTx(t *testing.T) {
 	pool := NewTransactionPool(*DefaultTxPoolConfig())
 	tx := newTestTx(t, 10, 100)
 
-	added, err := pool.AddTransaction(tx)
-	assert.Equal(t, added, true)
+	err := pool.AddTransaction(tx)
 	assert.Equal(t, err, error(nil))
 
-	added, err = pool.AddTransaction(tx)
-	assert.Equal(t, added, false)
+	err = pool.AddTransaction(tx)
 	assert.Equal(t, err, errTxHashExists)
 }
 
@@ -83,12 +79,10 @@ func Test_TransactionPool_Add_PoolFull(t *testing.T) {
 	tx1 := newTestTx(t, 10, 100)
 	tx2 := newTestTx(t, 20, 101)
 
-	added, err := pool.AddTransaction(tx1)
-	assert.Equal(t, added, true)
+	err := pool.AddTransaction(tx1)
 	assert.Equal(t, err, error(nil))
 
-	added, err = pool.AddTransaction(tx2)
-	assert.Equal(t, added, false)
+	err = pool.AddTransaction(tx2)
 	assert.Equal(t, err, errTxPoolFull)
 }
 
