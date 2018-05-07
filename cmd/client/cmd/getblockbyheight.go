f901634eb65e42c93d9fdcc5d285a87193cce62a
DuanWei <woshiduanweia@126.com>
2018-05-07 11:35:58 +0800

    add getblock rpc api & cmd (#140)
    * add rpc and cmd for getblockbyhash getblockbynumber getblocknumber
    
    * merge master
    
    * fix missing fullTx bug
    
    * Merge branch 'master' into dev
    
    * rename number to height
    
    * Merge branch 'master' into dev
    
    * format json,flag use bool
    
    * Merge branch 'master' into dev

---
+++ cmd/client/cmd/getblockbyheight.go
@@ -0,0 +1,63 @@
+/**
+*  @file
+*  @copyright defined in go-seele/LICENSE
+ */
+
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/rpc/jsonrpc"
+
+	"github.com/seeleteam/go-seele/seele"
+	"github.com/spf13/cobra"
+)
+
+var (
+	height *int64
+	tx     *bool
+)
+
+// getblockbyheightCmd represents the get block by height command
+var getblockbyheightCmd = &cobra.Command{
+	Use:   "getblockbyheight",
+	Short: "get block info by block height",
+	Long: `For example:
+	client.exe getblockbyheight --height -1 [-f=true] [-a 127.0.0.1:55027]`,
+	Run: func(cmd *cobra.Command, args []string) {
+		client, err := jsonrpc.Dial("tcp", rpcAddr)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer client.Close()
+
+		hashRequest := seele.GetBlockByHeightRequest{
+			Height: *height,
+			FullTx: *tx,
+		}
+		var result map[string]interface{}
+		err = client.Call("seele.GetBlockByHeight", &hashRequest, &result)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		jsonResult, err := json.MarshalIndent(&result, "", "\t")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("block :\n", string(jsonResult))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(getblockbyheightCmd)
+
+	height = getblockbyheightCmd.Flags().Int64("height", -1, "block height")
+	getblockbyheightCmd.MarkFlagRequired("height")
+
+	tx = getblockbyheightCmd.Flags().BoolP("fulltx", "f", false, "is add full tx, default is false")
+}
--- seele/api.go
+++ seele/api.go
@@ -9,6 +9,7 @@ import (
 	"math/big"
 
 	"github.com/seeleteam/go-seele/common"
+	"github.com/seeleteam/go-seele/common/hexutil"
 	"github.com/seeleteam/go-seele/core/types"
 	"github.com/seeleteam/go-seele/p2p"
 )
@@ -73,6 +74,117 @@ func (api *PublicSeeleAPI) GetAccountNonce(account *common.Address, nonce *uint6
 	*nonce = state.GetNonce(*account)
 
 	return nil
+}
+
+// GetBlockHeight get the block height of the chain head
+func (api *PublicSeeleAPI) GetBlockHeight(input interface{}, height *uint64) error {
+	block, _ := api.s.chain.CurrentBlock()
+	*height = block.Header.Height
+
+	return nil
+}
+
+// GetBlockByHeightRequest request param for GetBlockByHeight api
+type GetBlockByHeightRequest struct {
+	Height int64
+	FullTx bool
+}
+
+// GetBlockByHeight returns the requested block. When blockNr is -1 the chain head is returned. When fullTx is true all
+// transactions in the block are returned in full detail, otherwise only the transaction hash is returned
+func (api *PublicSeeleAPI) GetBlockByHeight(request *GetBlockByHeightRequest, result *map[string]interface{}) error {
+	store := api.s.chain.GetStore()
+	var block *types.Block
+	if request.Height < 0 {
+		block, _ = api.s.chain.CurrentBlock()
+	} else {
+		hash, err := store.GetBlockHash(uint64(request.Height))
+		if err != nil {
+			return err
+		}
+		var er error
+		block, er = store.GetBlock(hash)
+		if er != nil {
+			return er
+		}
+	}
+	response, err := rpcOutputBlock(block, request.FullTx)
+	if err != nil {
+		return err
+	}
+	*result = response
+	return nil
+}
+
+// GetBlockByHashRequest request param for GetBlockByHash api
+type GetBlockByHashRequest struct {
+	HashHex string
+	FullTx  bool
+}
+
+// GetBlockByHash returns the requested block. When fullTx is true all transactions in the block are returned in full
+// detail, otherwise only the transaction hash is returned
+func (api *PublicSeeleAPI) GetBlockByHash(request *GetBlockByHashRequest, result *map[string]interface{}) error {
+	store := api.s.chain.GetStore()
+	hashByte, err := hexutil.HexToBytes(request.HashHex)
+	if err != nil {
+		return err
+	}
+	hash := common.BytesToHash(hashByte)
+	block, err := store.GetBlock(hash)
+	if err != nil {
+		return err
+	}
+	response, err := rpcOutputBlock(block, request.FullTx)
+	if err != nil {
+		return err
+	}
+	*result = response
+	return nil
+}
+
+// rpcOutputBlock converts the given block to the RPC output which depends on fullTx
+func rpcOutputBlock(b *types.Block, fullTx bool) (map[string]interface{}, error) {
+	head := b.Header
+	fields := map[string]interface{}{
+		"height":     head.Height,
+		"hash":       b.HeaderHash.ToHex(),
+		"parentHash": head.PreviousBlockHash.ToHex(),
+		"nonce":      head.Nonce,
+		"stateHash":  head.StateHash.ToHex(),
+		"txHash":     head.TxHash.ToHex(),
+		"creator":    head.Creator.ToHex(),
+		"timestamp":  head.CreateTimestamp,
+		"difficulty": head.Difficulty,
+	}
+
+	formatTx := func(tx *types.Transaction) interface{} {
+		return tx.Hash.ToHex()
+	}
+
+	if fullTx {
+		formatTx = func(tx *types.Transaction) interface{} {
+			transaction := map[string]interface{}{
+				"hash":         tx.Hash.ToHex(),
+				"from":         tx.Data.From.ToHex(),
+				"to":           tx.Data.To.ToHex(),
+				"amount":       tx.Data.Amount,
+				"accountNonce": tx.Data.AccountNonce,
+				"payload":      tx.Data.Payload,
+				"timestamp":    tx.Data.Timestamp,
+			}
+			return transaction
+		}
+	}
+
+	txs := b.Transactions
+	transactions := make([]interface{}, len(txs))
+	for i, tx := range txs {
+		transactions[i] = formatTx(tx)
+	}
+	fields["transactions"] = transactions
+
+	return fields, nil
 }
 
 // PublicNetworkAPI provides an API to access network information.
