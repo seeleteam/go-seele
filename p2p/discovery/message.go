715a27a6807e6bf27875100852f40be73e8167f7
lucus <nikehao_love@163.com>
2018-05-18 13:31:04 +0800

    revert

--- p2p/discovery/message.go
+++ p2p/discovery/message.go
@@ -157,11 +157,7 @@ func (m *findNode) send(t *udp) {
 				return true
 			}
 
-<<<<<<< HEAD
 			t.log.Debug("got find response with %d nodes", len(r.Nodes))
-=======
-			t.log.Debug("find %d nodes", len(r.Nodes))
->>>>>>> master
 
 			found := false
 			for _, n := range r.Nodes {
