f2eed930e03565632233234ba33af731d79cc28d
Bo QIU <35757521+boqiu@users.noreply.github.com>
2018-05-22 16:32:02 +0800

    Statedb opt: 1) record first db err; 2) add deleted flag for state obj to avoid unnecessary query from trie (#194)
    * only set first error in statedb
    
    * add deleted flag for state object

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -83,6 +83,13 @@ func (s *Statedb) GetCopy() (*Statedb, error) {
 	}, nil
 }
 
+// setError only records the first error.
+func (s *Statedb) setError(err error) {
+	if s.dbErr == nil {
+		s.dbErr = err
+	}
+}
+
 // GetBalance returns the balance of the specified account if exists.
 // Otherwise, returns zero.
 func (s *Statedb) GetBalance(addr common.Address) *big.Int {
@@ -175,6 +182,7 @@ func (s *Statedb) commitOne(addr common.Address, obj *StateObject, batch databas
 
 	// Remove the account from state DB if suicided.
 	if obj.suicided {
+		obj.deleted = true
 		s.trie.Delete(addr.Bytes())
 	}
 
@@ -207,10 +215,13 @@ func (s *Statedb) GetOrNewStateObject(addr common.Address) *StateObject {
 }
 
 func (s *Statedb) getStateObject(addr common.Address) *StateObject {
-	value, ok := s.stateObjects.Get(addr)
-	if ok {
-		object := value.(*StateObject)
-		return object
+	if value, ok := s.stateObjects.Get(addr); ok {
+		if object := value.(*StateObject); !object.deleted {
+			return object
+		}
+
+		// object has already been deleted from trie.
+		return nil
 	}
 
 	object := newStateObject(addr)
@@ -222,6 +233,7 @@ func (s *Statedb) getStateObject(addr common.Address) *StateObject {
 	if err := common.Deserialize(val, &object.account); err != nil {
 		return nil
 	}
+
 	s.cache(addr, object)
 	return object
 }
