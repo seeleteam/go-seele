088dbe2e066ed07778661af69c1a1dacb522e91b
yanllearnn <yanllearnn@163.com>
2018-05-28 09:21:10 +0800

    change to easy

--- common/simple_decimal_test.go
+++ common/simple_decimal_test.go
@@ -12,41 +12,24 @@ import (
 )
 
 func Test_BigToDecimal(t *testing.T) {
-	var re string
-	var equal = false
 	var number = big.NewInt(100000000)
-	re = BigToDecimal(number)
-	equal = ("1" == re)
-	assert.Equal(t, equal, true)
+	assert.Equal(t, BigToDecimal(number), "1")
 
 	number = big.NewInt(0)
-	re = BigToDecimal(number)
-	equal = ("0" == re)
-	assert.Equal(t, equal, true)
+	assert.Equal(t, BigToDecimal(number), "0")
 
 	number = big.NewInt(12300)
-	re = BigToDecimal(number)
-	equal = ("0.000123" == re)
-	assert.Equal(t, equal, true)
+	assert.Equal(t, BigToDecimal(number), "0.000123")
 
 	number = big.NewInt(123)
-	re = BigToDecimal(number)
-	equal = ("0.00000123" == re)
-	assert.Equal(t, equal, true)
+	assert.Equal(t, BigToDecimal(number), "0.00000123")
 
 	number = big.NewInt(100012345678)
-	re = BigToDecimal(number)
-
-	equal = ("1000.12345678" == re)
-	assert.Equal(t, equal, true)
+	assert.Equal(t, BigToDecimal(number), "1000.12345678")
 
 	number = big.NewInt(10012345600)
-	re = BigToDecimal(number)
-	equal = ("100.123456" == re)
-	assert.Equal(t, equal, true)
+	assert.Equal(t, BigToDecimal(number), "100.123456")
 
 	number = big.NewInt(800000600)
-	re = BigToDecimal(number)
-	equal = ("8.000006" == re)
-	assert.Equal(t, equal, true)
+	assert.Equal(t, BigToDecimal(number), "8.000006")
 }
