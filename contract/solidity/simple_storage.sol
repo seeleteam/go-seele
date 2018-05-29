bb78c1258f80b6ad88b378276138010d6da3d578
qiubo <82121246@qq.com>
2018-05-29 19:03:44 +0800

    test SimpleStorage contract: create/get/set

---
+++ contract/solidity/simple_storage.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.0;
+
+contract SimpleStorage {
+    uint storedData;
+    
+    constructor() public {
+        storedData = 5;
+    }
+
+    function set(uint x) public {
+        storedData = x;
+    }
+
+    function get() public view returns (uint) {
+        return storedData;
+    }
+}
--- core/types/receipt.go
+++ core/types/receipt.go
@@ -15,11 +15,11 @@ var emptyReceiptRootHash = crypto.MustHash("empty receipt root hash")
 
 // Receipt represents the transaction processing receipt.
 type Receipt struct {
-	Result          []byte         // the execution result of the tx
-	PostState       common.Hash    // the root hash of the state trie after the tx is processed.
-	Logs            []*Log         // the log objects
-	TxHash          common.Hash    // the hash of the executed transaction
-	ContractAddress common.Address // Used when the tx (nil To address) is to create a contract.
+	Result          []byte      // the execution result of the tx
+	PostState       common.Hash // the root hash of the state trie after the tx is processed.
+	Logs            []*Log      // the log objects
+	TxHash          common.Hash // the hash of the executed transaction
+	ContractAddress []byte      // Used when the tx (nil To address) is to create a contract.
 }
 
 // CalculateHash calculates and returns the receipt hash.
