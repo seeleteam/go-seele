0b6becd22e0ed02fcd4e4f9fbbc85113f1932b71
weibogao <765993860@qq.com>
2018-06-07 11:23:30 +0800

    change runtime metrics (#249)
    * change runtime metrics

--- metrics/runtimemetrics.go
+++ metrics/runtimemetrics.go
@@ -18,15 +18,15 @@ func collectRuntimeMetrics() {
 		return
 	}
 
-	memAllocs := metrics.GetOrRegisterGauge("runtime.memory.allocs", metrics.DefaultRegistry)
-	memFrees := metrics.GetOrRegisterGauge("runtime.memory.frees", metrics.DefaultRegistry)
+	memAlloc := metrics.GetOrRegisterGauge("runtime.memory.alloc", metrics.DefaultRegistry)
+	memPauses := metrics.GetOrRegisterGauge("runtime.memory.pauses", metrics.DefaultRegistry)
 
 	memStats := new(runtime.MemStats)
 	// collect metrics
 	for {
 		runtime.ReadMemStats(memStats)
-		memAllocs.Update(int64(memStats.Mallocs))
-		memFrees.Update(int64(memStats.Frees))
+		memAlloc.Update(int64(memStats.Alloc))
+		memPauses.Update(int64(memStats.PauseTotalNs))
 
 		// sleep 5 seconds
 		time.Sleep(5 * time.Second)
