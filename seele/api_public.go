0c51a8620072f6250d94bdc1087615262b2373bc
sufay <sufay2830@gmail.com>
2018-06-11 13:55:09 +0800

    modify format

--- seele/api_public.go
+++ seele/api_public.go
@@ -86,7 +86,7 @@ func (api *PublicSeeleAPI) AddTx(tx *types.Transaction, result *bool) error {
 	shard := tx.Data.From.Shard()
 	var err error
 	if shard != common.LocalShardNumber {
-	        if err = tx.ValidateWithoutState(); err == nil {
+		if err = tx.ValidateWithoutState(); err == nil {
 			api.s.seeleProtocol.SendDifferentShardTx(tx, shard)
 		}
 	} else {
