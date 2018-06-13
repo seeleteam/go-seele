6cf240dadc0eadb9325ca48f7aacb7c7665a53ea
qiubo <82121246@qq.com>
2018-06-13 11:22:28 +0800

    add cmd to delete simulator db

--- cmd/vm/cmd/db.go
+++ cmd/vm/cmd/db.go
@@ -4,3 +4,28 @@
  */
 
 package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(dbCmd)
+}
+
+var dbCmd = &cobra.Command{
+	Use:   "initdb",
+	Short: "init or clean db",
+	Long:  "create a new db or clean the data of existing db for simulator",
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := os.RemoveAll(dir); err != nil {
+			fmt.Println("Failed to init db:", err.Error())
+			return
+		}
+
+		fmt.Println("Succeed to init db:", dir)
+	},
+}
