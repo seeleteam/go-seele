2167b2e1f2472a79339455e9022f5d3ab20b5a95
lucus <nikehao_love@163.com>
2018-06-15 11:17:43 +0800

    remove

--- cmd/node/run.bat
+++
@@ -1,4 +0,0 @@
-start node.exe start -c ..\tool\config\node1.json --accounts ..\tool\accounts.json
-ping -n 3 127.0.0.1>nul
-start node.exe start -c ..\tool\config\node2.json --accounts ..\tool\accounts.json
-start node.exe start -c ..\tool\config\node3.json --accounts ..\tool\accounts.json
