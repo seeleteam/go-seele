fe20614186bfbb21718f0bac83aa4ce4f1acd140
ypenghui6 <ypenghui6@163.com>
2018-06-22 19:10:45 +0800

    change through file to by parameter

--- cmd/client/cmd/signature.go
+++ cmd/client/cmd/signature.go
@@ -8,17 +8,16 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 
-	"github.com/seeleteam/go-seele/common"
+	"github.com/seeleteam/go-seele/cmd/util"
 	"github.com/seeleteam/go-seele/core/types"
 	"github.com/seeleteam/go-seele/crypto"
 	"github.com/seeleteam/go-seele/rpc"
 	"github.com/spf13/cobra"
 )
 
-var data *string
 var privateKey *string
+var param = util.TxInfo{}
 
 // signCmd represents the sign command
 var signCmd = &cobra.Command{
@@ -26,13 +25,8 @@ var signCmd = &cobra.Command{
 	Short: "sign the data with your private key",
 	Long: `sign the data with your private key
   For example:
-    client.exe sign -d datafile -k privatekey
-    client.exe sign -a 127.0.0.1:55027 -d datafile -k privatekey
-	the datafile like:
-	{	"From": "0x02235268262b72978c20eec2be8244b61dd5a0f1",
-		"To": "0x2a87b6504cd00af95a83b9887112016a2a991cf1",
-		"Amount": 10, "Fee": 1
-	}`,
+    client.exe sign -m 0 -t 0x<public address> -fee 0 -k privatekey
+    client.exe sign -a 127.0.0.1:55027 -m 0 -t 0x<public address> -fee 0 -k privatekey`,
 	Run: func(cmd *cobra.Command, args []string) {
 		client, err := rpc.Dial("tcp", rpcAddr)
 		if err != nil {
@@ -41,43 +35,19 @@ var signCmd = &cobra.Command{
 		}
 		defer client.Close()
 
-		var txd = types.TransactionData{}
-
-		content, err := ioutil.ReadFile(*data)
-		if err != nil {
-			fmt.Printf("read data file failed: %s\n", err.Error())
-			return
-		}
-		err = json.Unmarshal(content, &txd)
-		if err != nil {
-			fmt.Printf("data parsing error: %s\n", err.Error())
-			return
-		}
-
 		key, err := crypto.LoadECDSAFromString(*privateKey)
 		if err != nil {
 			fmt.Sprintf("load key failed %s", err)
 			return
 		}
 
-		fromAddr := crypto.GetAddress(&key.PublicKey)
-		if txd.From == common.EmptyAddress {
-			txd.From = *fromAddr
-		}
-
-		if txd.AccountNonce == 0 {
-			var nonce uint64
-			err = client.Call("seele.GetAccountNonce", fromAddr, &nonce)
-			if err != nil {
-				fmt.Printf("getting the sender account nonce failed: %s\n", err.Error())
-				return
-			}
-			fmt.Printf("got the sender account %s nonce: %d\n", fromAddr.ToHex(), nonce)
-			txd.AccountNonce = nonce + 1
+		txd, ok := util.CheckParameter(param, &key.PublicKey, client)
+		if !ok {
+			return
 		}
 
 		var tx = types.Transaction{}
-		tx.Data = txd
+		tx.Data = *txd
 		tx.Sign(key)
 
 		databytes, err := json.MarshalIndent(tx, "\t", "")
@@ -93,9 +63,17 @@ var signCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(signCmd)
 
-	data = signCmd.Flags().StringP("data", "d", "", "the transaction data file path, it's a json file, have four variable; From: account of payment, To: account to be credited, Amount: transfer amount, Fee: tip")
-	signCmd.MarkFlagRequired("data")
-
 	privateKey = signCmd.Flags().StringP("key", "k", "", "private key")
 	signCmd.MarkFlagRequired("key")
+
+	param.To = signCmd.Flags().StringP("to", "t", "", "public address of the receiver")
+	signCmd.MarkFlagRequired("to")
+
+	param.Amount = signCmd.Flags().StringP("amount", "m", "", "the amount of the transferred coins")
+	signCmd.MarkFlagRequired("amount")
+
+	param.Fee = signCmd.Flags().StringP("fee", "", "", "transaction fee")
+	signCmd.MarkFlagRequired("fee")
+
+	param.Payload = signCmd.Flags().StringP("payload", "", "", "transaction payload")
 }
