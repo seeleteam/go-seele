2195aac19ce004f20a2072c0816b788be819a8d5
ypenghui6 <ypenghui6@163.com>
2018-06-26 15:47:13 +0800

    fix bugs

--- metrics/runtimecpu.go
+++ metrics/runtimecpu.go
@@ -8,26 +8,8 @@ import (
 )
 
 var (
-	metricsCputMeter    = metrics.NewRegisteredMeter("cpu.accout", nil)
+	metricsCputGauge    = metrics.GetOrRegisterGauge("cpu.accout", nil)
 )
-
-//func getCPU() (int64,error){
-//	c := "top -d 1 | grep node | awk -F '[ ]+' '{print $9}'"
-//	cmd := exec.Command("sh", "-c", c)
-//	out, err := cmd.Output()
-//	fmt.Printf("cup data: %s\n", string(out))
-//	if err != nil {
-//		fmt.Printf("get cpu cmd failed: %s", err.Error())
-//		return *new(int64), err
-//	}
-//
-//	i, err := strconv.ParseInt(string(out), 10, 64)
-//	if err != nil{
-//		fmt.Printf("data type conversion failed: %s", err.Error())
-//		return *new(int64), err
-//	}
-//	return i, nil
-//}
 
 func getCPU(interval time.Duration, percpu bool) (int64,error){
 	out,err := cpu.Percent(interval, percpu)
@@ -40,20 +22,6 @@ func getCPU(interval time.Duration, percpu bool) (int64,error){
 		resul = resul + out[i - 1]
 	}
 	resul = resul / float64(len(out))
-
+	fmt.Printf("cup data: %v\n", resul)
 	return int64(resul),nil
-
-	//result := strconv.FormatFloat(resul, 'E', -1, 32)
-	//fmt.Printf("cup data: %s\n", result)
-	//if err != nil {
-	//	fmt.Printf("get cpu cmd failed: %s", err.Error())
-	//	return *new(int64), err
-	//}
-	//
-	//i, err := strconv.ParseInt(result, 10, 64)
-	//if err != nil{
-	//	fmt.Printf("data type conversion failed: %s", err.Error())
-	//	return *new(int64), err
-	//}
-	//return i, nil
 }
