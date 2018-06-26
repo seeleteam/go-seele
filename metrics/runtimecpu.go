f8199e62e9e87f264b13fa4a88eb5960c4d929bb
ypenghui6 <ypenghui6@163.com>
2018-06-26 13:31:43 +0800

    fix bugs

--- metrics/runtimecpu.go
+++ metrics/runtimecpu.go
@@ -4,20 +4,26 @@ import (
 	metrics "github.com/rcrowley/go-metrics"
 	"os/exec"
 	"fmt"
-	"encoding/binary")
+	"strconv"
+)
 
 var (
 	metricsCputMeter    = metrics.NewRegisteredMeter("cpu.accout", nil)
 )
 
-func getCPU() int64{
+func getCPU() (int64,error){
 	c := "top -d 1 | grep node | awk -F '[ ]+' '{print $10}'"
 	cmd := exec.Command("sh", "-c", c)
 	out, err := cmd.Output()
 	if err != nil {
-		fmt.Printf("cpu: %v", out)
-		return *new(int64)
+		fmt.Printf("get cpu cmd failed: %s", err.Error())
+		return *new(int64), err
 	}
 
-	return int64(binary.BigEndian.Uint64(out))
+	i, err := strconv.ParseInt(string(out), 10, 64)
+	if err != nil{
+		fmt.Printf("data type conversion failed: %s", err.Error())
+		return *new(int64), err
+	}
+	return i, nil
 }
