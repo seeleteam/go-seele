b4d8e015c9f20b9bce448e3299bba3922a9dcd3c
ypenghui6 <ypenghui6@163.com>
2018-06-26 11:25:53 +0800

    add cpu metrics

--- metrics/runtimecpu.go
+++ metrics/runtimecpu.go
@@ -4,17 +4,20 @@ import (
 	metrics "github.com/rcrowley/go-metrics"
 	"os/exec"
 	"fmt"
-)
+	"encoding/binary")
 
 var (
 	metricsCputMeter    = metrics.NewRegisteredMeter("cpu.accout", nil)
 )
 
-func getCPU(){
+func getCPU() int64{
 	c := "top -d 1 | grep node | awk -F '[ ]+' '{print $10}'"
 	cmd := exec.Command("sh", "-c", c)
 	out, err := cmd.Output()
 	if err != nil {
 		fmt.Printf("cpu: %v", out)
+		return *new(int64)
 	}
+
+	return int64(binary.BigEndian.Uint64(out))
 }
