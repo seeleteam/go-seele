d70a8407889a74962b6f5ea268168a81a16f36df
WTer <wesleyliu@qq.com>
2018-06-27 18:05:19 +0800

    Refine getacountshardnum command

--- cmd/client/cmd/getAccountShardNum.go
+++ cmd/client/cmd/getAccountShardNum.go
@@ -7,6 +7,7 @@ package cmd
 
 import (
 	"fmt"
+
 	"github.com/seeleteam/go-seele/common"
 	"github.com/spf13/cobra"
 )
@@ -14,14 +15,14 @@ import (
 var account *string
 
 // getacountshardnumcmd represents the get account shard number command
-var getacountshardnumcmd = &cobra.Command{
-	Use:   "getacountshardnumcmd",
+var getacountshardnumCmd = &cobra.Command{
+	Use:   "getacountshardnum",
 	Short: "get account shard number with specified account",
-	Long:  `get account shard number with specified account
+	Long: `get account shard number with specified account
 	For example:
-		client.exe getacountshardnumcmd --account 0x007d1b1ea335e8e4a74c0be781d828dc7db934b1`,
+		client.exe getacountshardnum --account 0x007d1b1ea335e8e4a74c0be781d828dc7db934b1`,
 	Run: func(cmd *cobra.Command, args []string) {
-		accountAddress,err := common.HexToAddress (*account)
+		accountAddress, err := common.HexToAddress(*account)
 		if err != nil {
 			fmt.Printf("the account is invalid for: %v\n", err)
 			return
@@ -33,6 +34,6 @@ var getacountshardnumcmd = &cobra.Command{
 }
 
 func init() {
-	account = getacountshardnumcmd.Flags().StringP("account", "", "", "account")
-	rootCmd.AddCommand(getacountshardnumcmd)
+	account = getacountshardnumCmd.Flags().StringP("account", "", "", "account")
+	rootCmd.AddCommand(getacountshardnumCmd)
 }
