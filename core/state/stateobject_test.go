dda0e22b78ca9a439f4808d0a28ff89b4c88fe14
Wang Feifan <wangfeifan@zsbatech.com>
2018-07-10 11:48:06 +0800

    set more value in sol1

--- core/state/stateobject_test.go
+++ core/state/stateobject_test.go
@@ -4,6 +4,10 @@ import (
 	"math/big"
 	"testing"
 
+	"github.com/seeleteam/go-seele/common"
+	"github.com/seeleteam/go-seele/database/leveldb"
+	"github.com/seeleteam/go-seele/trie"
+
 	"github.com/magiconair/properties/assert"
 	"github.com/seeleteam/go-seele/crypto"
 )
@@ -16,7 +20,7 @@ func Test_AccountClone(t *testing.T) {
 	a2 := a1.clone()
 	assert.Equal(t, a1, a2)
 
-	// The change in src value cannot change the value of desc
+	// The change in src value cannot change the value of dest
 	a1.Nonce = 2
 	assert.Equal(t, a1.Nonce != a2.Nonce, true)
 }
@@ -26,12 +30,21 @@ func Test_StateObjectClone(t *testing.T) {
 	addr := *crypto.MustGenerateRandomAddress()
 	so1 := newStateObject(addr)
 	so1.account = newTestAccount()
+	so1.code = []byte("contract code")
+	so1.dirtyCode = true
+	so1.suicided = true
+	so1.deleted = true
+
+	db, remove := leveldb.NewTestDatabase()
+	defer remove()
+	so1.storageTrie, _ = trie.NewTrie(common.EmptyHash, []byte("dbprefix"), db)
+	so1.cachedStorage[common.BytesToHash(so1.account.StorageRootHash)] = common.BytesToHash(so1.account.StorageRootHash)
 
 	// Function works well
 	so2 := so1.GetCopy()
 	assert.Equal(t, so1, so2)
 
-	// The change in src value cannot change the value of desc
+	// The change in src value cannot change the value of dest
 	so1.address = *crypto.MustGenerateRandomAddress()
 	assert.Equal(t, so1.address != so2.address, true)
 	assert.Equal(t, so1 != so2, true)
