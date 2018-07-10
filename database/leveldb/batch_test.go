7b7000e0d93e03ce25e4002801a7a604fc262bbb
WTer <wesleyliu@qq.com>
2018-07-10 16:05:07 +0800

    Add unittests for batch

--- database/leveldb/batch_test.go
+++ database/leveldb/batch_test.go
@@ -69,11 +69,21 @@ func Test_Batch_Delete(t *testing.T) {
 	batch.Put([]byte("2"), []byte("22"))
 	batch.Put([]byte("3"), []byte("33"))
 	batch.Delete([]byte("2"))
-	err := batch.Commit()
+
+	// not found, just still not write to disk
+	value, err := db.GetString("1")
+	assert.Equal(t, err, leveldb.ErrNotFound)
+	value, err = db.GetString("2")
+	assert.Equal(t, err, leveldb.ErrNotFound)
+
+	value, err = db.GetString("3")
+	assert.Equal(t, value, "")
+
+	err = batch.Commit()
 	if err != nil {
 		t.Fatalf("Failed to commit batch")
 	}
-	value, err := db.GetString("2")
+	value, err = db.GetString("2")
 	assert.Equal(t, err, leveldb.ErrNotFound)
 
 	value, err = db.GetString("3")
@@ -145,4 +155,70 @@ func Test_Batch_Rollback(t *testing.T) {
 
 	value, err := db.GetString("2")
 	assert.Equal(t, value, "22")
+}
+
+func Test_PutDeleteCommit(t *testing.T) {
+	// Init levelDB
+	db, remove := newTestLevelDBForBatch()
+	defer remove()
+
+	batch := db.NewBatch()
+	batch.Put([]byte("1"), []byte("11"))
+	batch.Put([]byte("2"), []byte("22"))
+	batch.Put([]byte("3"), []byte("33"))
+
+	// Not found due to still not commit
+	_, err := db.GetString("1")
+	assert.Equal(t, err, leveldb.ErrNotFound)
+	_, err = db.GetString("2")
+	assert.Equal(t, err, leveldb.ErrNotFound)
+	_, err = db.GetString("2")
+	assert.Equal(t, err, leveldb.ErrNotFound)
+
+	// Commit
+	err = batch.Commit()
+	if err != nil {
+		t.Fatalf("Failed to commit batch")
+	}
+
+	// All keys can be found after committed
+	value, err := db.GetString("1")
+	assert.Equal(t, err, nil)
+	assert.Equal(t, value, "11")
+
+	value, err = db.GetString("2")
+	assert.Equal(t, err, nil)
+	assert.Equal(t, value, "22")
+
+	value, err = db.GetString("3")
+	assert.Equal(t, err, nil)
+	assert.Equal(t, value, "33")
+
+	value, err = db.GetString("4")
+	assert.Equal(t, err, leveldb.ErrNotFound)
+
+	// Update
+	batch.Put([]byte("1"), []byte("1111"))
+	value, err = db.GetString("1")
+	assert.Equal(t, err, nil)
+	assert.Equal(t, value, "11")
+
+	err = batch.Commit()
+	if err != nil {
+		t.Fatalf("Failed to commit batch")
+	}
+
+	value, err = db.GetString("1")
+	assert.Equal(t, err, nil)
+	assert.Equal(t, value, "1111")
+
+	// Put empty key
+	batch.Put([]byte(""), []byte("I'm empty"))
+	err = batch.Commit()
+	if err != nil {
+		t.Fatalf("Failed to commit batch")
+	}
+	value, err = db.GetString("")
+	assert.Equal(t, err, nil)
+	assert.Equal(t, value, "I'm empty")
 }
