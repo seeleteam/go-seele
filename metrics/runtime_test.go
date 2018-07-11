27b3aab64f42c3a06e015c6307232f811c0d203f
ypenghui6 <ypenghui6@163.com>
2018-07-11 10:35:37 +0800

    fix bugs

--- metrics/runtime_test.go
+++ metrics/runtime_test.go
@@ -3,23 +3,21 @@ package metrics
 import (
 	"runtime"
 	"testing"
-	"time"
 
 	"github.com/magiconair/properties/assert"
 )
 
 func Test_getCPURate(t *testing.T) {
-	_, err := getCPURate(100*time.Millisecond, false)
+	_, err := getCPURate(0, false)
 	assert.Equal(t, err, nil)
 }
 
 func Test_getProcessCPURate(t *testing.T) {
-	_, err := getProcessCPURate(100 * time.Millisecond)
+	_, err := getProcessCPURate(0)
 	assert.Equal(t, err, nil)
 }
 
 func Test_GetDiskInfo(t *testing.T) {
-
 	if result := GetDiskInfo(); result == nil {
 		if runtime.GOOS == "linux" {
 			t.Fatal("get the linux machine disk info failed")
