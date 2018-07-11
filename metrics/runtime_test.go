52f885ed3ae6f703d91d3b9ae7bd4539fce36b47
ypenghui6 <ypenghui6@163.com>
2018-07-11 09:54:12 +0800

    add metric ut

--- metrics/runtime_test.go
+++ metrics/runtime_test.go
@@ -1,6 +1,7 @@
 package metrics
 
 import (
+	"runtime"
 	"testing"
 	"time"
 
@@ -10,4 +11,22 @@ import (
 func Test_getCPURate(t *testing.T) {
 	_, err := getCPURate(100*time.Millisecond, false)
 	assert.Equal(t, err, nil)
+}
+
+func Test_getProcessCPURate(t *testing.T) {
+	_, err := getProcessCPURate(100 * time.Millisecond)
+	assert.Equal(t, err, nil)
+}
+
+func Test_GetDiskInfo(t *testing.T) {
+
+	if result := GetDiskInfo(); result == nil {
+		if runtime.GOOS == "linux" {
+			t.Fatal("get disk info of linux machine failed")
+		}
+	} else {
+		if runtime.GOOS != "linux" {
+			t.Fatal("get disk info of windows machine failed")
+		}
+	}
 }
