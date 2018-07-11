01af5ea99dc718da748907d6586278b36d350931
ypenghui6 <ypenghui6@163.com>
2018-07-11 10:49:18 +0800

    add metric UT (#365)
    * add metric ut
    
    * fix comments

--- metrics/runtime_test.go
+++ metrics/runtime_test.go
@@ -1,13 +1,30 @@
 package metrics
 
 import (
+	"runtime"
 	"testing"
-	"time"
 
 	"github.com/magiconair/properties/assert"
 )
 
 func Test_getCPURate(t *testing.T) {
-	_, err := getCPURate(100*time.Millisecond, false)
+	_, err := getCPURate(0, false)
 	assert.Equal(t, err, nil)
+}
+
+func Test_getProcessCPURate(t *testing.T) {
+	_, err := getProcessCPURate(0)
+	assert.Equal(t, err, nil)
+}
+
+func Test_GetDiskInfo(t *testing.T) {
+	if result := GetDiskInfo(); result == nil {
+		if runtime.GOOS == "linux" {
+			t.Fatal("get the linux machine disk info failed")
+		}
+	} else {
+		if runtime.GOOS != "linux" {
+			t.Fatal("get the non linux machine disk info failed")
+		}
+	}
 }
