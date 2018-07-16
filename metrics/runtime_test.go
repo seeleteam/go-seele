88970928a22cb6da5b49275399f22e6717aef257
ypenghui6 <ypenghui6@163.com>
2018-07-16 17:39:50 +0800

    add UT and optimize code

--- metrics/runtime_test.go
+++ metrics/runtime_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/magiconair/properties/assert"
+	metric "github.com/rcrowley/go-metrics"
 )
 
 func Test_getCPURate(t *testing.T) {
@@ -26,5 +27,42 @@ func Test_GetDiskInfo(t *testing.T) {
 		if runtime.GOOS != "linux" {
 			t.Fatal("get the non linux machine disk info failed")
 		}
+	}
+}
+
+func Test_doMark(t *testing.T) {
+	registry := metric.DefaultRegistry
+	memStats := new(runtime.MemStats)
+	var lastPauseNs uint64
+	doMark(memStats, lastPauseNs)
+	if registry.Get("runtime.memory.alloc") == nil {
+		t.Fatal("get runtime.memory.alloc failed")
+	}
+	if registry.Get("runtime.memory.pauses") == nil {
+		t.Fatal("get runtime.memory.pauses failed")
+	}
+	if registry.Get("cpu.os") == nil {
+		t.Fatal("get cpu.os failed")
+	}
+	if registry.Get("cpu.seele") == nil {
+		t.Fatal("get cpu.seele failed")
+	}
+	if registry.Get("disk.free.count") == nil {
+		t.Fatal("get disk.free.count failed")
+	}
+	if registry.Get("disk.read.count") == nil {
+		t.Fatal("get disk.read.count failed")
+	}
+	if registry.Get("disk.write.count") == nil {
+		t.Fatal("get disk.write.count failed")
+	}
+	if registry.Get("disk.read.bytes") == nil {
+		t.Fatal("get disk.read.bytes failed")
+	}
+	if registry.Get("disk.write.bytes") == nil {
+		t.Fatal("get disk.write.bytes failed")
+	}
+	if registry.Get("not exsit") != nil {
+		t.Fatal("get a value of not exsit")
 	}
 }
