d8a75941a32d613c51ed445982f0771617ca386d
lucus <nikehao_love@163.com>
2018-08-01 10:55:22 +0800

    update rpc client about public api (#425)
    * update rpc client about public api
    
    * fix test

--- cmd/client2/flags.go
+++ cmd/client2/flags.go
@@ -26,9 +26,9 @@ var (
 		Destination: &accountValue,
 	}
 
-	heightValue uint64
-	heightFlag  = cli.Uint64Flag{
-		Name:        "height, h",
+	heightValue int64
+	heightFlag  = cli.Int64Flag{
+		Name:        "height",
 		Value:       0,
 		Usage:       "block height",
 		Destination: &heightValue,
@@ -39,5 +39,70 @@ var (
 		Name:        "fulltx, f",
 		Usage:       "whether print full transaction info",
 		Destination: &fulltxValue,
+	}
+
+	hashValue string
+	hashFlag  = cli.StringFlag{
+		Name:        "hash",
+		Usage:       "hash value in hex",
+		Destination: &hashValue,
+	}
+
+	fromValue string
+	fromFlag  = cli.StringFlag{
+		Name:        "from",
+		Usage:       "from address",
+		Destination: &fromValue,
+	}
+
+	toValue string
+	toFlag  = cli.StringFlag{
+		Name:        "to",
+		Usage:       "to address",
+		Destination: &toValue,
+	}
+
+	amountValue string
+	amountFlag  = cli.StringFlag{
+		Name:        "amount",
+		Usage:       "amount value, unit is fan",
+		Destination: &amountValue,
+	}
+
+	paloadValue string
+	paloadFlag  = cli.StringFlag{
+		Name:        "payload",
+		Value:       "",
+		Usage:       "transaction payload info",
+		Destination: &paloadValue,
+	}
+
+	feeValue string
+	feeFlag  = cli.StringFlag{
+		Name:        "fee",
+		Usage:       "transaction fee",
+		Destination: &feeValue,
+	}
+
+	nonceValue uint64
+	nonceFlag  = cli.Uint64Flag{
+		Name:        "nonce",
+		Value:       0,
+		Usage:       "transaction nonce",
+		Destination: &nonceValue,
+	}
+
+	contractValue string
+	contractFlag  = cli.StringFlag{
+		Name:        "contract",
+		Usage:       "contract code in hex",
+		Destination: &contractValue,
+	}
+
+	topicValue string
+	topicFlag  = cli.StringFlag{
+		Name:        "topic",
+		Usage:       "topic",
+		Destination: &topicValue,
 	}
 )
---
+++ cmd/client2/utils.go
@@ -0,0 +1,110 @@
+/**
+*  @file
+*  @copyright defined in go-seele/LICENSE
+ */
+
+package main
+
+import (
+	"crypto/ecdsa"
+	"fmt"
+	"math/big"
+
+	"github.com/seeleteam/go-seele/common"
+	"github.com/seeleteam/go-seele/common/hexutil"
+	"github.com/seeleteam/go-seele/core/types"
+	"github.com/seeleteam/go-seele/crypto"
+	"github.com/seeleteam/go-seele/rpc2"
+)
+
+const (
+	// DefaultNonce is the default value of nonce,when you are not set the nonce flag in client sendtx command by --nonce .
+	DefaultNonce uint64 = 0
+)
+
+func checkParameter(publicKey *ecdsa.PublicKey, client *rpc.Client) (*types.TransactionData, error) {
+	info := &types.TransactionData{}
+	var err error
+	if len(toValue) > 0 {
+		toAddr, err := common.HexToAddress(toValue)
+		if err != nil {
+			return info, fmt.Errorf("invalid receiver address: %s", err)
+		}
+		info.To = toAddr
+	}
+
+	amount, ok := big.NewInt(0).SetString(amountValue, 10)
+	if !ok {
+		return info, fmt.Errorf("invalid amount value")
+	}
+	info.Amount = amount
+
+	fee, ok := big.NewInt(0).SetString(feeValue, 10)
+	if !ok {
+		return info, fmt.Errorf("invalid fee value")
+	}
+	info.Fee = fee
+
+	fromAddr := crypto.GetAddress(publicKey)
+	info.From = *fromAddr
+
+	nonce, err := GetAccountNonce(client, *fromAddr)
+	if err != nil {
+		return info, fmt.Errorf("failed to get the sender account nonce: %s", err)
+	}
+
+	if nonceValue == nonce || nonceValue == DefaultNonce {
+		info.AccountNonce = nonce
+	} else {
+		if nonceValue < nonce {
+			return info, fmt.Errorf("your nonce is: %d,current nonce is: %d,you must set your nonce greater than or equal to current nonce", nonceValue, nonce)
+		}
+		info.AccountNonce = nonceValue
+	}
+
+	fmt.Printf("account %s current nonce: %d, sending nonce: %d\n", fromAddr.ToHex(), nonce, info.AccountNonce)
+
+	payload := []byte(nil)
+	if len(paloadValue) > 0 {
+		if payload, err = hexutil.HexToBytes(paloadValue); err != nil {
+			return info, fmt.Errorf("invalid payload, %s", err)
+		}
+	}
+	info.Payload = payload
+
+	return info, nil
+}
+
+// GetAccountNonce get account nonce by account
+func GetAccountNonce(client *rpc.Client, account common.Address) (uint64, error) {
+	var nonce uint64
+	err := client.Call(&nonce, "seele_getAccountNonce", account)
+	return nonce, err
+}
+
+func generateTx(from *ecdsa.PrivateKey, to common.Address, amount *big.Int, fee *big.Int, nonce uint64, payload []byte) (*types.Transaction, error) {
+	fromAddr := crypto.GetAddress(&from.PublicKey)
+
+	var tx *types.Transaction
+	var err error
+	if to.IsEmpty() {
+		tx, err = types.NewContractTransaction(*fromAddr, amount, fee, nonce, payload)
+	} else {
+		switch to.Type() {
+		case common.AddressTypeExternal:
+			tx, err = types.NewTransaction(*fromAddr, to, amount, fee, nonce)
+		case common.AddressTypeContract:
+			tx, err = types.NewMessageTransaction(*fromAddr, to, amount, fee, nonce, payload)
+		default:
+			return nil, fmt.Errorf("unsupported address type: %d", to.Type())
+
+		}
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("create transaction err %s", err)
+	}
+	tx.Sign(from)
+
+	return tx, nil
+}
