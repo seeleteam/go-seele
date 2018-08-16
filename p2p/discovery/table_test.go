a4c5b07f5f632982100ff9a0aed3bce5486cded0
ypenghui6 <ypenghui6@163.com>
2018-08-16 10:29:56 +0800

    add table unit test

--- p2p/discovery/table_test.go
+++ p2p/discovery/table_test.go
@@ -54,3 +54,33 @@ func Test_addNode(t *testing.T) {
 	assert.Equal(t, len(table.shardBuckets[3].peers) != 1, true)
 	assert.Equal(t, table.shardBuckets[2].peers[0] == node2, true)
 }
+
+
+func Test_findNodeWithTarget(t *testing.T) {
+	id1 := "snode://0101f3c956d0a320b153a097c3d04efa488d43d7@127.0.0.1:9000[1]"
+	node1, err := NewNodeFromString(id1)
+	assert.Equal(t, err, nil)
+
+	add2 := common.HexMustToAddres("0xe58010916a17a5d333814f8bae82db6cb6b7ab81")
+	id2 := fmt.Sprintf("snode://%v%v", hex.EncodeToString(add2.Bytes()), "@127.0.0.1:9888[2]")
+	node2, err := NewNodeFromString(id2)
+	assert.Equal(t, err, nil)
+
+	table := newTestTable()
+	table.addNode(node1)
+	table.addNode(node2)
+
+	nodes := table.findNodeWithTarget(node1.getSha())
+	assert.Equal(t, len(nodes), 1)
+	assert.Equal(t, nodes[0] == node1, true)
+
+	nodes1 := table.findNodeWithTarget(node2.getSha())
+	assert.Equal(t, len(nodes1), 1)
+	assert.Equal(t, nodes1[0] != node2, true)
+
+	noExistKey := common.HexMustToAddres("0xfbe506bdaf256682551873290d0a794d51bac4d1")
+	noExistId := fmt.Sprintf("snode://%v%v", hex.EncodeToString(noExistKey.Bytes()), "@127.0.0.1:9888[2]")
+	noExistNode, err := NewNodeFromString(noExistId)
+	nodes2 := table.findNodeWithTarget(noExistNode.getSha())
+	assert.Equal(t, len(nodes2), 0)
+}
