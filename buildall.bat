4fa6acebb5573100d25b03df0d506c9cd0028f40
wxcsdb88 <wx_cs_db_88@163.com>
2018-08-21 21:58:18 +0800

    refactor the compile script for windows platform

--- buildall.bat
+++ buildall.bat
@@ -1,24 +1,67 @@
-@echo off 
+@echo off
 goto comment
     Build the command lines and tests in Windows.
     Must install gcc tool before building.
 :comment
 
-echo on
+set para=%*
+if not defined para (
+    set act=all
+)else (
+    set act=%*
+)
 
+for %%i in (%act%) do (
+    call :%%i
+)  
+pause
+exit
+
+:all
+call :discovery
+call :node
+call :client
+call :tool
+call :vm
+goto:eof
+
+:discovery
+echo on
 go build -o ./build/discovery.exe ./cmd/discovery
 @echo "Done discovery building"
+@echo off
+goto:eof
 
-go build -o ./build/node.exe ./cmd/node 
+:node
+echo on
+go build -o ./build/node.exe ./cmd/node
 @echo "Done node building"
+@echo off
+goto:eof
 
+:client
+echo on
 go build -o ./build/client.exe ./cmd/client
 @echo "Done client building"
+@echo off
+goto:eof
 
+:tool
+echo on
 go build -o ./build/tool.exe ./cmd/tool
 @echo "Done tool building"
+@echo off
+goto:eof
 
+:vm
+echo on
 go build -o ./build/vm.exe ./cmd/vm
 @echo "Done vm building"
+@echo off
+goto:eof
 
-pause
+:clean
+del build\* /q /f /s
+@echo "Done clean the build dir"
+@echo off
+goto:eof
