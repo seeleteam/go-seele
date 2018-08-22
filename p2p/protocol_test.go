69140d0b7c064fcf78838567fb81f7732a7695ea
WTer <wesleyliu@qq.com>
2018-08-22 10:20:17 +0800

    add unittests for p2p.protocol

---
+++ p2p/protocol_test.go
@@ -0,0 +1,37 @@
+/**
+*  @file
+*  @copyright defined in go-seele/LICENSE
+ */
+
+package p2p
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/magiconair/properties/assert"
+)
+
+func Test_Protocol_Cap(t *testing.T) {
+	protocol := newProtocol()
+	cap := protocol.cap()
+
+	assert.Equal(t, cap.Name, protocol.Name)
+	assert.Equal(t, cap.Version, protocol.Version)
+}
+
+func Test_Protocol_String(t *testing.T) {
+	protocol := newProtocol()
+	cap := protocol.cap()
+	str := cap.String()
+
+	assert.Equal(t, strings.Compare(str, "udp/1"), 0)
+}
+
+func newProtocol() *Protocol {
+	return &Protocol{
+		Name:    "udp",
+		Version: 1,
+		Length:  1048,
+	}
+}
