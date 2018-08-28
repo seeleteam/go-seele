0e73b0d734425002441d37b371e1d40f0dac9c58
qiubo <82121246@qq.com>
2018-08-28 11:49:00 +0800

    implement the sub-chain system contract

--- contract/system/sub_chain.go
+++ contract/system/sub_chain.go
@@ -5,6 +5,12 @@
 
 package system
 
+import (
+	"encoding/json"
+
+	"github.com/seeleteam/go-seele/common"
+)
+
 const (
 	cmdSubChainRegister byte = iota // register a sub-chain.
 	cmdSubChainQuery
@@ -20,10 +26,50 @@ var (
 	}
 )
 
+// SubChainInfo represents the sub-chain registration information.
+type SubChainInfo struct {
+	Name        string
+	Version     string
+	StaticNodes []string
+
+	TokenFullName  string
+	TokenShortName string
+	TokenAmount    uint64
+
+	GenesisDifficulty uint64
+	GenesisAccounts   map[common.Address]uint64
+}
+
 func registerSubChain(jsonRegInfo []byte, context *Context) ([]byte, error) {
+	var info SubChainInfo
+	if err := json.Unmarshal(jsonRegInfo, &info); err != nil {
+		return nil, err
+	}
+
+	key, err := domainNameToKey([]byte(info.Name))
+	if err != nil {
+		return nil, err
+	}
+
+	if value := context.statedb.GetData(subChainContractAddress, key); len(value) > 0 {
+		return nil, errExists
+	}
+
+	value, err := json.MarshalIndent(info, "", "\t")
+	if err != nil {
+		return nil, err
+	}
+
+	context.statedb.SetData(subChainContractAddress, key, value)
+
 	return nil, nil
 }
 
 func querySubChain(subChainName []byte, context *Context) ([]byte, error) {
-	return nil, nil
+	key, err := domainNameToKey(subChainName)
+	if err != nil {
+		return nil, err
+	}
+
+	return context.statedb.GetData(subChainContractAddress, key), nil
 }
