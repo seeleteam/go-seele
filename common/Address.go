89d8a71e92ac08de041653ecf36a48650129cade
Wang Feifan <wangfeifan@zsbatech.com>
2018-08-31 17:24:39 +0800

    optimize the IsReserved logic

--- common/Address.go
+++ common/Address.go
@@ -84,11 +84,7 @@ func (id *Address) Type() AddressType {
 
 // IsReserved returns true if the address is reserved
 func (id *Address) IsReserved() bool {
-	if !id.IsEmpty() && bytes.Compare(id.Bytes(), MaxSystemContractAddress.Bytes()) <= 0 {
-		return true
-	}
-
-	return false
+	return !id.IsEmpty() && bytes.Compare(id.Bytes(), MaxSystemContractAddress.Bytes()) <= 0
 }
 
 // Bytes get the actual bytes
