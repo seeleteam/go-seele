3382522d2f105e9cc1989b386be23e532b1bfefb
WTer <wesleyliu@qq.com>
2018-09-04 14:37:43 +0800

    different error message in Windows and Linux when port rebinding

--- p2p/discovery/net_test.go
+++ p2p/discovery/net_test.go
@@ -6,7 +6,6 @@
 package discovery
 
 import (
-	"fmt"
 	"net"
 	"strings"
 	"testing"
@@ -23,16 +22,12 @@ func Test_Net_GetUDPConn(t *testing.T) {
 	conn, err := getUDPConn(addr)
 	defer conn.Close()
 
-	fmt.Println("getUDPConn 0 err:", err)
 	assert.Equal(t, err, nil)
-	fmt.Println("getUDPConn 0 conn:", conn)
 	assert.Equal(t, conn != nil, true)
 
 	// failed to listen due to already binded
 	conn, err = getUDPConn(addr)
-	fmt.Println("getUDPConn 1 err:", err)
 	assert.Equal(t, err != nil, true)
-	assert.Equal(t, strings.Contains(err.Error(), "bind: Only one usage of each socket address"), true)
-	fmt.Println("getUDPConn 1 conn:", conn)
+	assert.Equal(t, strings.Contains(err.Error(), "bind:"), true)
 	assert.Equal(t, conn == nil, true)
 }
