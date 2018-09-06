c862d1bbbd2f49c7de2c446471a068f9c532e96c
Feifan Wang <35793667+wangff15386@users.noreply.github.com>
2018-09-06 13:53:24 +0800

    Abstract common statedb operation, demoting the vm processing transactions (#511)
    * Abstract common statedb operation, releasing the weight of svm processing transactions
    
    * change the factory class to factory method
    
    * remove the svm interface
    
    * fix a bug
    
    * fix conflicts
    
    * Merge branch 'master' into add-svm-factory
    
    * fix conflicts
    
    * fix conflicts
    
    * rename economic.go to fee.go

--- core/svm/svm_test.go
+++ core/svm/svm_test.go
@@ -9,38 +9,119 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/seeleteam/go-seele/common"
 	"github.com/seeleteam/go-seele/common/hexutil"
 	"github.com/seeleteam/go-seele/core/state"
 	"github.com/seeleteam/go-seele/core/store"
 	"github.com/seeleteam/go-seele/core/types"
+	"github.com/seeleteam/go-seele/core/vm"
 	"github.com/seeleteam/go-seele/crypto"
 	"github.com/seeleteam/go-seele/database/leveldb"
+	"github.com/stretchr/testify/assert"
 )
 
-func Test_NewSeeleVM(t *testing.T) {
-	statedb, bcStore, address, dispose := preprocessContract(1000*common.SeeleToFan.Uint64(), 38)
-	defer dispose()
-
-	header := newTestBlockHeader()
-	// Create contract tx, please refer to the code content in contract/solidity/simple_storage.sol
-	code := mustHexToBytes("0x608060405234801561001057600080fd5b50600560008190555060df806100276000396000f3006080604052600436106049576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff16806360fe47b114604e5780636d4ce63c146078575b600080fd5b348015605957600080fd5b5060766004803603810190808035906020019092919050505060a0565b005b348015608357600080fd5b50608a60aa565b6040518082815260200191505060405180910390f35b8060008190555050565b600080549050905600a165627a7a723058207f6dc43a0d648e9f5a0cad5071cde46657de72eb87ab4cded53a7f1090f51e6d0029")
-	createContractTx, err := types.NewContractTransaction(address, new(big.Int), big.NewInt(1), 38, code)
+func Test_Process_EVM(t *testing.T) {
+	ctx, err := newTestContext(t, big.NewInt(0))
 	assert.Equal(t, err, nil)
 
-	ctx := &Context{
-		Tx:          createContractTx,
-		Statedb:     statedb,
-		BlockHeader: header,
-		BcStore:     bcStore,
-	}
-	svm := NewSeeleVM(ctx)
-	receipt, err := svm.Process(createContractTx, 8)
+	receipt, err := Process(ctx)
 	assert.Equal(t, err, nil)
 	assert.Equal(t, receipt.Failed, false)
-	assert.Equal(t, receipt.TxHash, createContractTx.CalculateHash())
-	assert.Equal(t, receipt.ContractAddress, crypto.CreateAddress(address, 38).Bytes())
+	assert.Equal(t, receipt.TxHash, ctx.Tx.CalculateHash())
+	assert.Equal(t, receipt.ContractAddress, crypto.CreateAddress(ctx.Tx.Data.From, ctx.Tx.Data.AccountNonce).Bytes())
+
+	// nonce + 1
+	nonce := ctx.Statedb.GetNonce(ctx.Tx.Data.From)
+	assert.Equal(t, nonce, ctx.Tx.Data.AccountNonce+1)
+
+	// add fee to coinbase and sub fee from tx.from
+	balanceC := ctx.Statedb.GetBalance(ctx.BlockHeader.Creator)
+	assert.Equal(t, big.NewInt(0).SetUint64(receipt.TotalFee), balanceC)
+
+	balanceF := ctx.Statedb.GetBalance(ctx.Tx.Data.From)
+	assert.Equal(t, big.NewInt(0).Sub(big.NewInt(0).SetUint64(fromBalance-receipt.TotalFee), ctx.Tx.Data.Amount), balanceF)
+
+	// postState
+	postState, err1 := ctx.Statedb.Hash()
+	assert.Equal(t, err1, nil)
+	assert.Equal(t, postState, receipt.PostState)
+
+	// logs
+	logs := make([]*types.Log, 0)
+	assert.Equal(t, logs, receipt.Logs)
+}
+
+func Test_Porcess_SysContract(t *testing.T) {
+	// CreateDomainName
+	ctx, _ := newTestContext(t, big.NewInt(0))
+	byteD := []byte{0}
+	ctx.Tx.Data.Payload = append(byteD, []byte("seele.fan")...) // 0x007365656c652e66616e
+	ctx.Tx.Data.To = common.BytesToAddress([]byte{1, 1})        // 0x0000000000000000000000000000000000000101
+
+	receipt, err := Process(ctx)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, false, receipt.Failed)
+	assert.Equal(t, ctx.Tx.Data.To.Bytes(), receipt.ContractAddress)
+	assert.Equal(t, ctx.Tx.Hash, receipt.TxHash)
+
+	gasCreateDomainName := uint64(50000) // gas used to create a domain name
+	assert.Equal(t, receipt.UsedGas, gasCreateDomainName)
+	assert.Equal(t, new(big.Int).SetUint64(receipt.TotalFee), new(big.Int).Add(usedGasFee(gasCreateDomainName), ctx.Tx.Data.Fee))
+
+	// DomainNameCreator
+	ctx1 := ctx
+	byteD = []byte{1}
+	ctx1.Tx.Data.Payload = append(byteD, []byte("seele.fan")...) // 0x017365656c652e66616e
+
+	receipt1, err1 := Process(ctx1)
+	assert.Equal(t, nil, err1)
+	assert.Equal(t, false, receipt1.Failed)
+	assert.Equal(t, ctx1.Tx.Data.To.Bytes(), receipt1.ContractAddress)
+	assert.Equal(t, ctx1.Tx.Hash, receipt1.TxHash)
+
+	gasDomainNameCreator := uint64(100000) // gas used to query the creator of given domain name
+	assert.Equal(t, receipt1.UsedGas, gasDomainNameCreator)
+	assert.Equal(t, new(big.Int).SetUint64(receipt1.TotalFee), new(big.Int).Add(usedGasFee(gasDomainNameCreator), ctx1.Tx.Data.Fee))
+}
+
+func Test_Process_ErrInsufficientBalance(t *testing.T) {
+	// get the tx total fee
+	ctx, _ := newTestContext(t, big.NewInt(1))
+	receipt, err := Process(ctx)
+	assert.Equal(t, err, nil)
+	totalFee := receipt.TotalFee
+
+	// cannot apply the tx
+	ctx1, _ := newTestContext(t, big.NewInt(1))
+	balanceF1 := big.NewInt(0)
+	ctx1.Statedb.SetBalance(ctx1.Tx.Data.From, balanceF1)
+	receipt1, err1 := Process(ctx1)
+	assert.Equal(t, err1, vm.ErrInsufficientBalance)
+	assert.Empty(t, receipt1)
+
+	// can apply the tx but not enough fee
+	ctx2, _ := newTestContext(t, big.NewInt(1))
+	balanceF2 := big.NewInt(0).Sub(big.NewInt(0).SetUint64(totalFee), ctx2.Tx.Data.Fee)
+	ctx2.Statedb.SetBalance(ctx2.Tx.Data.From, balanceF2)
+	receipt2, err2 := Process(ctx2)
+	assert.Equal(t, err2, vm.ErrInsufficientBalance)
+	assert.Empty(t, receipt2)
+
+	// nonce not changed
+	nonce1 := ctx1.Statedb.GetNonce(ctx1.Tx.Data.From)
+	assert.Equal(t, nonce1, ctx1.Tx.Data.AccountNonce)
+	nonce2 := ctx2.Statedb.GetNonce(ctx2.Tx.Data.From)
+	assert.Equal(t, nonce2, ctx2.Tx.Data.AccountNonce)
+
+	// balance not changed
+	balanceC1 := ctx1.Statedb.GetBalance(ctx1.BlockHeader.Creator)
+	assert.Equal(t, balanceC1, big.NewInt(0))
+	balanceF1Now := ctx1.Statedb.GetBalance(ctx1.Tx.Data.From)
+	assert.Equal(t, balanceF1Now, balanceF1)
+	balanceC2 := ctx2.Statedb.GetBalance(ctx2.BlockHeader.Creator)
+	assert.Equal(t, balanceC2, big.NewInt(0))
+	balanceF2Now := ctx2.Statedb.GetBalance(ctx2.Tx.Data.From)
+	assert.Equal(t, balanceF2Now, balanceF2)
 }
 
 func mustHexToBytes(hex string) []byte {
@@ -74,10 +155,10 @@ func preprocessContract(balance, nonce uint64) (*state.Statedb, store.Blockchain
 	}
 }
 
-func newTestBlockHeader() *types.BlockHeader {
+func newTestBlockHeader(coinbase common.Address) *types.BlockHeader {
 	return &types.BlockHeader{
 		PreviousBlockHash: crypto.MustHash("block previous hash"),
-		Creator:           *crypto.MustGenerateRandomAddress(),
+		Creator:           coinbase,
 		StateHash:         crypto.MustHash("state root hash"),
 		TxHash:            crypto.MustHash("tx root hash"),
 		ReceiptHash:       crypto.MustHash("receipt root hash"),
@@ -86,5 +167,52 @@ func newTestBlockHeader() *types.BlockHeader {
 		CreateTimestamp:   big.NewInt(time.Now().Unix()),
 		Nonce:             10,
 		ExtraData:         make([]byte, 0),
+	}
+}
+
+var fromBalance = uint64(1000 * common.SeeleToFan.Uint64())
+
+func newTestContext(t *testing.T, amount *big.Int) (*Context, error) {
+	statedb, bcStore, address, dispose := preprocessContract(fromBalance, 38)
+	defer dispose()
+
+	coinbase := *crypto.MustGenerateRandomAddress()
+	statedb.CreateAccount(coinbase)
+	header := newTestBlockHeader(coinbase)
+	// Create contract tx, please refer to the code content in contract/solidity/simple_storage.sol
+	code := mustHexToBytes("0x608060405234801561001057600080fd5b50600560008190555060df806100276000396000f3006080604052600436106049576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff16806360fe47b114604e5780636d4ce63c146078575b600080fd5b348015605957600080fd5b5060766004803603810190808035906020019092919050505060a0565b005b348015608357600080fd5b50608a60aa565b6040518082815260200191505060405180910390f35b8060008190555050565b600080549050905600a165627a7a723058207f6dc43a0d648e9f5a0cad5071cde46657de72eb87ab4cded53a7f1090f51e6d0029")
+	tx, err := types.NewContractTransaction(address, amount, big.NewInt(1), 38, code)
+
+	return &Context{
+		Tx:          tx,
+		TxIndex:     8,
+		Statedb:     statedb,
+		BlockHeader: header,
+		BcStore:     bcStore,
+	}, err
+}
+
+func Benchmark_CreateContract_EVM(b *testing.B) {
+	ctx, _ := newTestContext(nil, big.NewInt(0))
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Process(ctx)
+	}
+}
+
+func Benchmark_CallContract_EVM(b *testing.B) {
+	ctx, _ := newTestContext(nil, big.NewInt(0))
+	receipt, _ := Process(ctx)
+	contractAddr := common.BytesToAddress(receipt.ContractAddress)
+
+	// Call contract tx: SimpleStorage.get(), it returns 5 as initialized in constructor.
+	input := mustHexToBytes("0x6d4ce63c")
+	amount, fee, nonce := big.NewInt(0), big.NewInt(1), uint64(38)
+	callContractTx, _ := types.NewMessageTransaction(ctx.Tx.Data.From, contractAddr, amount, fee, nonce, input)
+	ctx.Tx = callContractTx
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		Process(ctx)
 	}
 }
