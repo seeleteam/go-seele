ef56e1a2743908440570d0c3c25daa93ca65e69b
ypenghui6 <ypenghui6@163.com>
2018-09-11 18:36:19 +0800

    delete seele repetitive api

--- seele/api_debug.go
+++ seele/api_debug.go
@@ -36,42 +36,6 @@ func (api *PrivateDebugAPI) PrintBlock(height int64) (*types.Block, error) {
 	return block, nil
 }
 
-// GetTxPoolContent returns the transactions contained within the transaction pool
-func (api *PrivateDebugAPI) GetTxPoolContent() (map[string][]map[string]interface{}, error) {
-	txPool := api.s.TxPool()
-	data := txPool.GetTransactions(false, true)
-
-	content := make(map[string][]map[string]interface{})
-	for _, tx := range data {
-		key := tx.Data.From.ToHex()
-		content[key] = append(content[key], PrintableOutputTx(tx))
-	}
-
-	return content, nil
-}
-
-// GetTxPoolTxCount returns the number of transaction in the pool
-func (api *PrivateDebugAPI) GetTxPoolTxCount() (uint64, error) {
-	txPool := api.s.TxPool()
-	return uint64(txPool.GetPendingTxCount()), nil
-}
-
-// GetPendingTransactions returns all pending transactions
-func (api *PrivateDebugAPI) GetPendingTransactions() ([]map[string]interface{}, error) {
-	pendingTxs := api.s.TxPool().GetTransactions(true, true)
-	transactions := make([]map[string]interface{}, 0)
-	for _, tx := range pendingTxs {
-		transactions = append(transactions, PrintableOutputTx(tx))
-	}
-
-	return transactions, nil
-}
-
-// GetPendingDebts returns all pending debts
-func (api *PrivateDebugAPI) GetPendingDebts() ([]*types.Debt, error) {
-	return api.s.DebtPool().GetAll(), nil
-}
-
 // TpsInfo tps detail info
 type TpsInfo struct {
 	StartHeight uint64
