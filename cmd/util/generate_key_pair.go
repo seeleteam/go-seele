66780b692ca37e7db87c9bda6f2fe06531ef2b0b
zhousk <846838816@qq.com>
2018-09-20 15:58:10 +0800

    using common generate key method in client commend (#604)
    * using common generate key method in client commend

--- cmd/util/generate_key_pair.go
+++ cmd/util/generate_key_pair.go
@@ -24,19 +24,10 @@ func GetGenerateKeyPairCmd(name string) (cmds *cobra.Command) {
 		Short: "generate a key pair with specified shard number",
 		Long:  "generate a key pair and print them with hex values\n For example:\n" + name + " key --shard 1",
 		Run: func(cmd *cobra.Command, args []string) {
-			var publicKey *common.Address
-			var privateKey *ecdsa.PrivateKey
-			var err error
-			if *shard > common.ShardCount {
-				fmt.Printf("not supported shard number, shard number should be [0, %d]\n", common.ShardCount)
+			publicKey, privateKey, err := GenerateKey(*shard)
+			if err != nil {
+				fmt.Println(err)
 				return
-			} else if *shard == 0 {
-				publicKey, privateKey, err = crypto.GenerateKeyPair()
-				if err != nil {
-					fmt.Printf("failed to generate the key pair: %s\n", err.Error())
-				}
-			} else {
-				publicKey, privateKey = crypto.MustGenerateShardKeyPair(*shard)
 			}
 
 			fmt.Printf("public key:  %s\n", publicKey.ToHex())
