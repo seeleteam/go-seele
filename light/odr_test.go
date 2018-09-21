61a62e61afa065a3b76a5776c97b164f6e03239a
WTer <wesleyliu@qq.com>
2018-09-21 15:58:54 +0800

    add unittests for odr

---
+++ light/odr_test.go
@@ -0,0 +1,38 @@
+/**
+*  @file
+*  @copyright defined in go-seele/LICENSE
+ */
+
+package light
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Odr_AllFuncs(t *testing.T) {
+	id := uint32(1)
+	err := ""
+
+	odr := newOdrItem(id, err)
+	assert.Equal(t, odr.getRequestID(), id)
+	assert.Nil(t, odr.getError())
+
+	id = uint32(2)
+	odr.setRequestID(id)
+	assert.Equal(t, odr.getRequestID(), id)
+	assert.Nil(t, odr.getError())
+
+	errString := "something unexpected"
+	odr = newOdrItem(id, errString)
+	assert.Equal(t, odr.getError(), errors.New(errString))
+}
+
+func newOdrItem(id uint32, err string) *odrItem {
+	return &odrItem{
+		ReqID: id,
+		Error: err,
+	}
+}
