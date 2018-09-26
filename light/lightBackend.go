8cbd175165a6f654a6b4f3c33197ef4f6d9e5a85
Wensi Liu <wensiliu@seelenet.com>
2018-09-26 14:11:13 +0800

    fix coding style issues and add comments for light.backend (#628)
    * fix coding style issues and add comments for light.backend

--- light/lightBackend.go
+++ light/lightBackend.go
@@ -12,28 +12,38 @@ import (
 	"github.com/seeleteam/go-seele/p2p"
 )
 
+// LightBackend represents a channel (client) that communicate with backend node service.
 type LightBackend struct {
 	s *ServiceClient
 }
 
+// NewLightBackend creates a LightBackend
 func NewLightBackend(s *ServiceClient) *LightBackend {
 	return &LightBackend{s}
 }
 
+// TxPoolBackend gets the instance of tx pool
 func (l *LightBackend) TxPoolBackend() api.Pool { return l.s.txPool }
 
+// GetNetVersion gets the network version
 func (l *LightBackend) GetNetVersion() string { return l.s.netVersion }
 
+// GetNetWorkID gets the network id
 func (l *LightBackend) GetNetWorkID() string { return l.s.networkID }
 
+// GetP2pServer gets instance of p2pServer
 func (l *LightBackend) GetP2pServer() *p2p.Server { return l.s.p2pServer }
 
+// ChainBackend gets instance of blockchain
 func (l *LightBackend) ChainBackend() api.Chain { return l.s.chain }
 
+// Log gets instance of log
 func (l *LightBackend) Log() *log.SeeleLog { return l.s.log }
 
+// ProtocolBackend gets instance of seeleProtocol
 func (l *LightBackend) ProtocolBackend() api.Protocol { return l.s.seeleProtocol }
 
+// GetBlock gets a specific block through block's hash and height
 func (l *LightBackend) GetBlock(hash common.Hash, height int64) (*types.Block, error) {
 	var request *odrBlock
 	request = &odrBlock{Height: height, Hash: hash}
@@ -48,10 +58,12 @@ func (l *LightBackend) GetBlock(hash common.Hash, height int64) (*types.Block, e
 	return request.Block, nil
 }
 
+// GetBlockTotalDifficulty gets total difficulty by block hash
 func (l *LightBackend) GetBlockTotalDifficulty(hash common.Hash) (*big.Int, error) {
 	return l.ChainBackend().GetStore().GetBlockTotalDifficulty(hash)
 }
 
+// GetReceiptByTxHash gets block's receipt by block hash
 func (l *LightBackend) GetReceiptByTxHash(hash common.Hash) (*types.Receipt, error) {
 	var request *odrtReceipt
 	request = &odrtReceipt{TxHash: hash}
@@ -66,6 +78,7 @@ func (l *LightBackend) GetReceiptByTxHash(hash common.Hash) (*types.Receipt, err
 	return request.Receipt, nil
 }
 
+// GetTransaction gets tx, block index and its debt by tx hash
 func (l *LightBackend) GetTransaction(pool api.PoolCore, bcStore store.BlockchainStore, txHash common.Hash) (*types.Transaction, *api.BlockIndex, *types.Debt, error) {
 	request := &odrTxByHash{TxHash: txHash}
 	if err := l.s.odrBackend.sendRequest(request); err != nil {
