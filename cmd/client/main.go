edb43fe39cdfc8ae992744f91dba13b9c9645b45
Wang Feifan <wangfeifan@zsbatech.com>
2018-09-28 18:44:44 +0800

    seperate new app code

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -10,22 +10,10 @@ import (
 	"os"
 
 	"github.com/seeleteam/go-seele/cmd/client/cmd"
-	"github.com/urfave/cli"
 )
 
 func main() {
-	app := cli.NewApp()
-	app.Name = "full node client"
-	app.Usage = "interact with full node process"
-	app.HideVersion = true
-	app.Authors = []cli.Author{
-		cli.Author{
-			Name:  "seeleteam",
-			Email: "dev@seelenet.com",
-		},
-	}
-
-	cmd.AddCommands(app, true)
+	app := cmd.NewApp(true)
 
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
