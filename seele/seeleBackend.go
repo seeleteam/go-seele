7cec63e6b1f8034951e5e5e17cbb0dfa22a3f2ea
zhousk <846838816@qq.com>
2018-09-28 13:47:50 +0800

    fix getblock rpc (#639)
    * fix getblock rpc
    
    * add unit test for seelebackend

--- seele/seeleBackend.go
+++ seele/seeleBackend.go
@@ -33,11 +33,11 @@ func (sd *SeeleBackend) Log() *log.SeeleLog { return sd.s.log }
 
 func (sd *SeeleBackend) ProtocolBackend() api.Protocol { return sd.s.seeleProtocol }
 
-// GetBlockByHeightOrHash returns the requested block.
+// GetBlock returns the requested block by hash or height
 func (sd *SeeleBackend) GetBlock(hash common.Hash, height int64) (*types.Block, error) {
 	var block *types.Block
 	var err error
-	if hash.IsEmpty() {
+	if !hash.IsEmpty() {
 		store := sd.s.chain.GetStore()
 		block, err = store.GetBlock(hash)
 		if err != nil {
--- seele/seeleBackend_test.go
+++ seele/seeleBackend_test.go
@@ -1,3 +1,8 @@
+/**
+*  @file
+*  @copyright defined in go-seele/LICENSE
+ */
+
 package seele
 
 import (
@@ -16,6 +21,29 @@ import (
 	"github.com/seeleteam/go-seele/log"
 	"github.com/stretchr/testify/assert"
 )
+
+func newTestSeeleBackend() *SeeleBackend {
+	seeleService := newTestSeeleService()
+	return &SeeleBackend{seeleService}
+}
+
+func Test_SeeleBackend_GetBlock(t *testing.T) {
+	seeleBackend := newTestSeeleBackend()
+	defer seeleBackend.s.Stop()
+
+	block, err := seeleBackend.GetBlock(common.EmptyHash, -1)
+	assert.Equal(t, err, nil)
+	assert.Equal(t, block.Header.Height, uint64(0))
+
+	block1, err := seeleBackend.GetBlock(block.HeaderHash, -1)
+	assert.Equal(t, err, nil)
+	assert.Equal(t, block1.HeaderHash, block.HeaderHash)
+
+	block2, err := seeleBackend.GetBlock(common.EmptyHash, 0)
+	assert.Equal(t, err, nil)
+	assert.Equal(t, block2.Header.Height, uint64(0))
+	assert.Equal(t, block2.HeaderHash, block.HeaderHash)
+}
 
 func Test_GetReceiptByHash(t *testing.T) {
 	dbPath := filepath.Join(common.GetTempFolder(), ".GetReceiptByHash")
