04b303e32efe51f9aac85db5f257821b294118f3
Feifan Wang <35793667+wangff15386@users.noreply.github.com>
2018-10-10 13:32:17 +0800

    Update abi rpc api to make seelejs call better (#677)
    * update abi rpc-api to make seelejs call better
    
    * update comments

--- seele/api_abi.go
+++ seele/api_abi.go
@@ -12,37 +12,47 @@ import (
 	"github.com/seeleteam/go-seele/accounts/abi"
 	"github.com/seeleteam/go-seele/accounts/abi/bind"
 	"github.com/seeleteam/go-seele/common"
+	"github.com/seeleteam/go-seele/common/hexutil"
 )
 
 // KeyABIHash is the hash key to storing abi to statedb
 var KeyABIHash = common.StringToHash("KeyABIHash")
 
-// GeneratePayload according to abi to generate payload
-func (api *PublicSeeleAPI) GeneratePayload(parsed abi.ABI, methodName string, args ...interface{}) ([]byte, error) {
+// GeneratePayload according to abi json string and methodName and args to generate payload hex string
+func (api *PublicSeeleAPI) GeneratePayload(abiJSON string, methodName string, args ...interface{}) (string, error) {
+	parsed, err := abi.JSON(strings.NewReader(abiJSON))
+	if err != nil {
+		return "", fmt.Errorf("invalid abiJSON '%s', err: %s", abiJSON, err)
+	}
+
 	method, exist := parsed.Methods[methodName]
 	if !exist {
-		return nil, fmt.Errorf("method '%s' not found", methodName)
+		return "", fmt.Errorf("method '%s' not found", methodName)
 	}
 
 	if ok, err := bind.CheckInputArgs(method.Inputs, args...); !ok {
-		return nil, err
+		return "", err
 	}
 
-	return parsed.Pack(methodName, args...)
+	bytes, err := parsed.Pack(methodName, args...)
+	if err != nil {
+		return "", err
+	}
+
+	return hexutil.BytesToHex(bytes), nil
 }
 
-// GetABI according to contract address to get abi
-func (api *PublicSeeleAPI) GetABI(contractAddr common.Address) (abi.ABI, error) {
-	parsed := abi.ABI{}
+// GetABI according to contract address to get abi json string
+func (api *PublicSeeleAPI) GetABI(contractAddr common.Address) (string, error) {
 	statedb, err := api.s.chain.GetCurrentState()
 	if err != nil {
-		return parsed, err
+		return "", err
 	}
 
 	abiBytes := statedb.GetData(contractAddr, KeyABIHash)
 	if len(abiBytes) == 0 {
-		return parsed, fmt.Errorf("the abi of contract '%s' not found", contractAddr.ToHex())
+		return "", fmt.Errorf("the abi of contract '%s' not found", contractAddr.ToHex())
 	}
 
-	return abi.JSON(strings.NewReader(string(abiBytes)))
+	return string(abiBytes), nil
 }
