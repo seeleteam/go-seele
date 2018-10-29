7edb376b98613c00e06dbb64d8e71b70fb676de3
Bo QIU <35757521+boqiu@users.noreply.github.com>
2018-10-29 10:59:34 +0800

    txpool discard low price txs when full (#713)
    * add ut for txCollection
    
    * txpool pending queue support to discard low priority txs
    
    * add peek worst tx API
    
    * enable txpool to discard low price txs when full
    
    * fix comments

--- core/tx_collection_test.go
+++ core/tx_collection_test.go
@@ -6,10 +6,12 @@
 package core
 
 import (
+	"math/big"
 	"testing"
+	"time"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/seeleteam/go-seele/core/types"
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_txCollection_add(t *testing.T) {
@@ -53,7 +55,7 @@ func Test_txCollection_get(t *testing.T) {
 	assert.Equal(t, collection.get(6) == nil, true)
 }
 
-func Test_txCollection_Remove(t *testing.T) {
+func Test_txCollection_remove(t *testing.T) {
 	collection := newTxCollection()
 
 	assert.Equal(t, len(collection.list()), 0)
@@ -117,4 +119,50 @@ func Test_txCollection_pop(t *testing.T) {
 	// pop tx2, nonce = 4
 	assert.Equal(t, collection.pop(), tx2)
 	assert.Equal(t, collection.len(), 0)
+}
+
+func Test_txCollection_cmp(t *testing.T) {
+	// compare with nil collection
+	assert.Equal(t, 1, newTxCollection().cmp(nil))
+
+	// compare 2 empty collections
+	c1, c2 := newTxCollection(), newTxCollection()
+	assert.Equal(t, 0, c1.cmp(c2))
+
+	// compare with empty collection
+	tx1 := newTestPoolTx(t, 1, 3)
+	tx1.Data.GasPrice = big.NewInt(2)
+	c1.add(tx1)
+	assert.Equal(t, 1, c1.cmp(c2))
+
+	// compare with lower price collection
+	tx2 := newTestPoolTx(t, 1, 3)
+	tx2.Data.GasPrice = big.NewInt(1)
+	c2.add(tx2)
+	assert.Equal(t, 1, c1.cmp(c2))
+
+	// compare with higher price collection
+	c2.pop()
+	tx2.Data.GasPrice = big.NewInt(3)
+	c2.add(tx2)
+	assert.Equal(t, -1, c1.cmp(c2))
+
+	// compare with same price, but earlier timestamp
+	c2.pop()
+	tx2.Data.GasPrice = big.NewInt(2)
+	tx2.timestamp = tx1.timestamp.Add(-time.Second)
+	c2.add(tx2)
+	assert.Equal(t, -1, c1.cmp(c2))
+
+	// compare with same price, but later timestamp
+	c2.pop()
+	tx2.timestamp = tx1.timestamp.Add(time.Second)
+	c2.add(tx2)
+	assert.Equal(t, 1, c1.cmp(c2))
+
+	// compare with same price and timestamp
+	c2.pop()
+	tx2.timestamp = tx1.timestamp
+	c2.add(tx2)
+	assert.Equal(t, 0, c1.cmp(c2))
 }
