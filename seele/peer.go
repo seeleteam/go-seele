dd83674bc51de0d4eed0c5b366834595a7a5b45b
yanllearnn <yanllearnn@163.com>
2018-11-06 17:06:11 +0800

    print log when error occurs

--- seele/peer.go
+++ seele/peer.go
@@ -237,7 +237,10 @@ func (p *peer) sendBlockHeaders(magic uint32, headers []*types.BlockHeader) erro
 	buff := common.SerializePanic(sendMsg)
 
 	p.log.Debug("peer send [downloader.BlockHeadersMsg] with length %d size %d byte peerid:%s", len(headers), len(buff), p.peerStrID)
-	return p2p.SendMessage(p.rw, downloader.BlockHeadersMsg, buff)
+	err := p2p.SendMessage(p.rw, downloader.BlockHeadersMsg, buff)
+	p.log.Debug("peer send [downloader.BlockHeadersMsg] err=%s", err)
+
+	return err
 }
 
 // RequestBlocksByHashOrNumber fetches a batch of blocks corresponding to the
@@ -267,7 +270,10 @@ func (p *peer) sendBlocks(magic uint32, blocks []*types.Block) error {
 	buff := common.SerializePanic(sendMsg)
 
 	p.log.Debug("peer send [downloader.BlocksMsg] with length: %d, size:%d byte peerid:%s", len(blocks), len(buff), p.peerStrID)
-	return p2p.SendMessage(p.rw, downloader.BlocksMsg, buff)
+	err := p2p.SendMessage(p.rw, downloader.BlocksMsg, buff)
+	p.log.Debug("peer send [downloader.BlocksMsg] err=%s", err)
+
+	return err
 }
 
 func (p *peer) sendHeadStatus(msg *chainHeadStatus) error {
