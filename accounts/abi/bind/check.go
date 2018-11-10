606bf12883a1da3723523b6c540ad8a631757bc7
Wang Feifan <wangfeifan@zsbatech.com>
2018-11-10 12:08:27 +0800

    add api parse types

--- accounts/abi/bind/check.go
+++ accounts/abi/bind/check.go
@@ -6,8 +6,10 @@
 package bind
 
 import (
+	"errors"
 	"fmt"
 	"math/big"
+	"strconv"
 	"strings"
 
 	"github.com/seeleteam/go-seele/accounts/abi"
@@ -42,24 +44,91 @@ func parseArg(abiType string, arg string) (interface{}, error) {
 		if err != nil {
 			return nil, err
 		}
-
 		return common.BytesToAddress(bytes), nil
 	case "*big.Int":
 		number, ok := big.NewInt(0).SetString(arg, 10)
 		if !ok {
 			return nil, fmt.Errorf("number[%v] parsed error ", number)
 		}
-
 		return number, nil
 	case "bool":
 		if arg == "true" {
 			return true, nil
 		}
-
 		return false, nil
+	case "int8":
+		number, err := strconv.Atoi(arg)
+		if err != nil {
+			return nil, err
+		}
+		return int8(number), nil
+	case "int16":
+		number, err := strconv.Atoi(arg)
+		if err != nil {
+			return nil, err
+		}
+		return int16(number), nil
+	case "int32":
+		number, err := strconv.Atoi(arg)
+		if err != nil {
+			return nil, err
+		}
+		return int32(number), nil
+	case "int64":
+		number, err := strconv.Atoi(arg)
+		if err != nil {
+			return nil, err
+		}
+		return int64(number), nil
+	case "uint8":
+		number, err := strconv.Atoi(arg)
+		if err != nil {
+			return nil, err
+		}
+		return uint8(number), nil
+	case "uint16":
+		number, err := strconv.Atoi(arg)
+		if err != nil {
+			return nil, err
+		}
+		return uint16(number), nil
+	case "uint32":
+		number, err := strconv.Atoi(arg)
+		if err != nil {
+			return nil, err
+		}
+		return uint32(number), nil
+	case "uint64":
+		number, err := strconv.Atoi(arg)
+		if err != nil {
+			return nil, err
+		}
+		return uint64(number), nil
 	default:
-		if strings.Contains(arg, "]byte") {
-			return []byte(arg), nil
+		if strings.Contains(abiType, "]byte") {
+			fmt.Println("abiType:", abiType)
+			fmt.Println("arg:", arg)
+			bytes, err := hexutil.HexToBytes(arg)
+			if err != nil {
+				return nil, err
+			}
+			length := abiType[1 : len(abiType)-5]
+			if length == "" {
+				return bytes, nil
+			}
+			// b := [l]byte{}
+			switch length {
+			case "32":
+				var b [32]byte
+				for index, count := len(bytes)-1, len(b)-1; index >= 0 && count >= 0; {
+					b[count] = bytes[index]
+					index--
+					count--
+				}
+				return b, nil
+			default:
+				return nil, errors.New("Now abi only supports bytes32 and bytes, and it will totally support in seele.js")
+			}
 		}
 
 		return arg, nil
