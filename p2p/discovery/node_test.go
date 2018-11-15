2b63569bd0c00304bd4ce65af967a485bd8976cf
qiubo <82121246@qq.com>
2018-11-15 15:02:32 +0800

    fix discovery failed uts

--- p2p/discovery/node_test.go
+++ p2p/discovery/node_test.go
@@ -13,13 +13,13 @@ import (
 )
 
 func Test_NodeId(t *testing.T) {
-	id := "snode://c3d04efa488d43d7d7e05a44791492c9979ff558@192.168.122.132:9000[1]"
+	id := "snode://c3d04efa488d43d7d7e05a44791492c9979ff551@192.168.122.132:9000[1]"
 
 	node, err := NewNodeFromString(id)
 	assert.Equal(t, err, nil)
 
 	hex := hex.EncodeToString(node.ID.Bytes())
-	assert.Equal(t, hex, "c3d04efa488d43d7d7e05a44791492c9979ff558")
+	assert.Equal(t, hex, "c3d04efa488d43d7d7e05a44791492c9979ff551")
 	assert.Equal(t, node.IP.String(), "192.168.122.132")
 	assert.Equal(t, node.UDPPort, 9000)
 	assert.Equal(t, node.Shard, uint(1))
--- p2p/discovery/server_test.go
+++ p2p/discovery/server_test.go
@@ -9,13 +9,13 @@ import (
 	"net"
 	"testing"
 
+	"github.com/seeleteam/go-seele/crypto"
 	"github.com/stretchr/testify/assert"
-	"github.com/seeleteam/go-seele/common"
 )
 
 func Test_Server_StartService(t *testing.T) {
 	nodeDir := "."
-	myID := common.HexMustToAddres("0xd0c549b022f5a17a8f50a4a448d20ba579d01781")
+	myID := *crypto.MustGenerateShardAddress(1)
 	myAddr, _ := net.ResolveUDPAddr("udp", "127.0.0.1:9777")
 	bootstrap := make([]*Node, 0)
 	shard := uint(1)
