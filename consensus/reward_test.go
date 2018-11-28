c782c678f9dca3da34c3a0fc70d8324a3e74e3cf
lucus <nikehao_love@163.com>
2018-11-28 15:57:29 +0800

    use latest model

--- consensus/reward_test.go
+++ consensus/reward_test.go
@@ -6,7 +6,6 @@
 package consensus
 
 import (
-	"fmt"
 	"math/big"
 	"testing"
 
@@ -33,12 +32,10 @@ func Test_RewardTotal(t *testing.T) {
 	for i := 0; i < 10; i++ {
 		reward := GetReward(uint64(i)*blockNumberPerEra + 1)
 		rewardYear := new(big.Int).Mul(reward, new(big.Int).SetUint64(blockNumberPerEra))
-		fmt.Println(rewardYear)
 		sum = new(big.Int).Add(sum, rewardYear)
 	}
 
-	fmt.Println(sum)
-	fmt.Println(targetReward)
+	sum = new(big.Int).Mul(sum, big.NewInt(common.ShardCount))
 
 	duration := new(big.Int).Div(targetReward, big.NewInt(10))
 	assert.True(t, sum.Cmp(new(big.Int).Add(targetReward, duration)) < 0)
