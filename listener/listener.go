dda9d886bbb5930f523dffc0dc6556d2b3a0cf31
forjoin92 <forjoin92@gmail.com>
2018-11-29 23:32:00 +0800

    optimize code

--- listener/listener.go
+++ listener/listener.go
@@ -6,10 +6,8 @@
 package listener
 
 import (
-	"fmt"
 	"io/ioutil"
 	"strings"
-	"sync/atomic"
 
 	"github.com/seeleteam/go-seele/accounts/abi"
 	"github.com/seeleteam/go-seele/common/errors"
@@ -17,11 +15,8 @@ import (
 )
 
 var (
-	// ErrInvalidArguments is returned when NewListener arguments are invalid.
+	// ErrInvalidArguments is returned when NewContractEventABI arguments are invalid.
 	ErrInvalidArguments = errors.New("the abiPath and eventName should be not empty")
-
-	// ErrListenerIsRunning is returned when start the Listener but the Listener is running.
-	ErrListenerIsRunning = errors.New("listener is running")
 
 	// ErrNoEvent is returned when the event is not in the specified abi.
 	ErrNoEvent = errors.New("This contract has no such event")
@@ -29,85 +24,48 @@ var (
 
 var log = seelelog.GetLogger("eventListener")
 
-// Listener represents contract event listener.
-type Listener struct {
-	running int32
+// ContractEventABI represents contract event parser.
+type ContractEventABI struct {
+	// the map key is topic, the value is event name,
+	// topic to compare with log's topic,
+	// event name to label the event how to deal.
+	topicEventNames map[string]string
 
-	abiPath    string
-	eventNames []string
-	topics     map[string]struct{}
-	parser     abi.ABI
+	parser abi.ABI
 }
 
-func (l *Listener) String() string {
-	var topics []string
-	for topic := range l.topics {
-		topics = append(topics, topic)
-	}
-
-	return strings.Join(topics, ",")
-}
-
-// NewListener returns a Listener instance.
-func NewListener(abiPath string, eventNames ...string) (*Listener, error) {
+// NewContractEventABI returns a ContractEventABI instance.
+func NewContractEventABI(abiPath string, eventNames ...string) (*ContractEventABI, error) {
 	if abiPath == "" || len(eventNames) == 0 {
 		return nil, ErrInvalidArguments
 	}
 
-	log.Info("New Listener with abi path: %s, events: %v", abiPath, eventNames)
-
-	return &Listener{
-		abiPath:    abiPath,
-		eventNames: eventNames,
-		topics:     make(map[string]struct{}),
-	}, nil
-}
-
-// Start parses the abi info to topics and parser, and begin the listener.
-func (l *Listener) Start() error {
-	if atomic.LoadInt32(&l.running) == 1 {
-		log.Info("Listener is running")
-		return ErrListenerIsRunning
-	}
-
-	if !atomic.CompareAndSwapInt32(&l.running, 0, 1) {
-		log.Info("Another goroutine has already started the listener")
-		return nil
-	}
-
-	file, err := ioutil.ReadFile(l.abiPath)
+	file, err := ioutil.ReadFile(abiPath)
 	if err != nil {
-		atomic.StoreInt32(&l.running, 0)
-		return fmt.Errorf("failed to read abi file, err: %s", err.Error())
+		return nil, errors.NewStackedError(err, "failed to read abi file")
 	}
 
 	parser, err := abi.JSON(strings.NewReader(string(file)))
 	if err != nil {
-		atomic.StoreInt32(&l.running, 0)
-		return errors.NewStackedError(err, "failed to parse abi")
+		return nil, errors.NewStackedError(err, "failed to parse abi")
 	}
 
-	l.parser = parser
+	c := &ContractEventABI{
+		topicEventNames: make(map[string]string),
+	}
+
+	c.parser = parser
 
 	var topic string
-	for _, eventName := range l.eventNames {
+	for _, eventName := range eventNames {
 		event, ok := parser.Events[eventName]
 		if !ok {
-			atomic.StoreInt32(&l.running, 0)
-			log.Error("This contract: %s, has no such event: %s", l.abiPath, eventName)
-			return ErrNoEvent
+			log.Error("This contract: %s, has no such event: %s", abiPath, eventName)
+			return nil, ErrNoEvent
 		}
 		topic = event.Id().Hex()
-		l.topics[topic] = struct{}{}
+		c.topicEventNames[topic] = eventName
 	}
 
-	log.Info("Listener running with abi path: %s, topics: %v", l.abiPath, l)
-
-	return nil
-}
-
-// Stop is used to stop the Listener
-func (l *Listener) Stop() {
-	atomic.StoreInt32(&l.running, 0)
-	l.topics = make(map[string]struct{})
+	return c, nil
 }
--- listener/listener_test.go
+++ listener/listener_test.go
@@ -17,81 +17,25 @@ const path1 = `/testConfig/SimpleEventTest1.abi`
 const getX = "getX"
 const getY = "getY"
 
-const path2 = `/testConfig/SimpleEventTest2.abi`
-const getA = "getA"
-const getB = "getB"
+const getXTopic = "0x672e793f48f65acb771442258a567e553d1620c0684e1cbd9fe06ee380d1b642"
+const getYTopic = "0x1086821eef716a909c39f2efe1e810bcd29246a6da19d04f9fc3f8d2889392e5"
 
-const patherr = `SimpleEventTest.abi`
-
-const getATopic = "0xa0acb9dd79e9d920ef642cb67cc5040eb54b29b163936c05777853bc5f4772b0"
-const getBTopic = "0xa1c51915e437ec30e58312c6ff1ae0b5e7fc72426b83ddac06c2431e9edc5da1"
-
-func Test_NewListener(t *testing.T) {
+func Test_NewContractEventABI(t *testing.T) {
 	currentProjectPath, err := os.Getwd()
 	assert.NoError(t, err)
 	configFilePath1 := filepath.Join(currentProjectPath, path1)
 
 	// empty abi path
-	_, err = NewListener("")
+	_, err = NewContractEventABI("")
 	assert.Equal(t, err, ErrInvalidArguments)
 	// empty events
-	_, err = NewListener(configFilePath1)
+	_, err = NewContractEventABI(configFilePath1)
 	assert.Equal(t, err, ErrInvalidArguments)
 	// valid arguments
-	l, err := NewListener(configFilePath1, getX, getY)
+	c, err := NewContractEventABI(configFilePath1, getX, getY)
 	assert.NoError(t, err)
-	assert.Equal(t, l.abiPath, configFilePath1)
-	assert.Contains(t, l.eventNames, getX)
-	assert.Contains(t, l.eventNames, getY)
-}
-
-func Test_Listener_Start(t *testing.T) {
-	currentProjectPath, err := os.Getwd()
-	assert.NoError(t, err)
-	configFilePath1 := filepath.Join(currentProjectPath, path1)
-	configFilePath2 := filepath.Join(currentProjectPath, path2)
-
-	// empty event
-	l, err := NewListener(configFilePath1, getX, "")
-	assert.NoError(t, err)
-	assert.Equal(t, l.Start(), ErrNoEvent)
-	assert.Equal(t, l.running, int32(0))
-
-	// already running Listener
-	l, err = NewListener(configFilePath1, getX)
-	assert.NoError(t, err)
-	l.running = 1
-	assert.Equal(t, l.Start(), ErrListenerIsRunning)
-
-	// error abi path
-	l, err = NewListener(patherr, getB)
-	assert.NoError(t, err)
-	err = l.Start()
-	assert.Error(t, err)
-	assert.Equal(t, l.running, int32(0))
-
-	// valid arguments
-	l, err = NewListener(configFilePath2, getA, getB)
-	assert.NoError(t, err)
-	assert.NoError(t, l.Start())
-	assert.Equal(t, l.running, int32(1))
-	var topics []string
-	for topic := range l.topics {
-		topics = append(topics, topic)
-	}
-	assert.Contains(t, topics, getATopic)
-	assert.Contains(t, topics, getBTopic)
-}
-
-func Test_Listener_Stop(t *testing.T) {
-	currentProjectPath, err := os.Getwd()
-	assert.NoError(t, err)
-	configFilePath2 := filepath.Join(currentProjectPath, path2)
-	l, err := NewListener(configFilePath2, getA, getB)
-	assert.NoError(t, err)
-	assert.NoError(t, l.Start())
-	assert.Equal(t, l.running, int32(1))
-	l.Stop()
-	assert.Equal(t, l.running, int32(0))
-	assert.Equal(t, len(l.topics), 0)
+	topicEventNames := make(map[string]string)
+	topicEventNames[getXTopic] = getX
+	topicEventNames[getYTopic] = getY
+	assert.Equal(t, c.topicEventNames, topicEventNames)
 }
