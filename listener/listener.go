9e82d33d8638aea17db167b91b441f31556e580e
wangyanyu <forjoin92@gmail.com>
2018-11-30 16:44:30 +0800

    add contract event listener (#834)
    * add contract event listener
    
    * optimize code
    
    * Merge branch 'master' into event
    
    * optimize code
    
    * remove unused error info
    
    * Update listener.go

---
+++ listener/listener.go
@@ -0,0 +1,65 @@
+/**
+*  @file
+*  @copyright defined in go-seele/LICENSE
+ */
+
+package listener
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+
+	"github.com/seeleteam/go-seele/accounts/abi"
+	"github.com/seeleteam/go-seele/common/errors"
+)
+
+var (
+	// ErrInvalidArguments is returned when NewContractEventABI arguments are invalid.
+	ErrInvalidArguments = errors.New("the abiPath and eventName should not be empty")
+)
+
+// ContractEventABI represents contract event parser.
+type ContractEventABI struct {
+	// the map key is topic, the value is event name,
+	// topic to compare with log's topic,
+	// event name to label the event how to deal.
+	topicEventNames map[string]string
+
+	parser abi.ABI
+}
+
+// NewContractEventABI returns a ContractEventABI instance.
+func NewContractEventABI(abiPath string, eventNames ...string) (*ContractEventABI, error) {
+	if len(abiPath) == 0 || len(eventNames) == 0 {
+		return nil, ErrInvalidArguments
+	}
+
+	file, err := ioutil.ReadFile(abiPath)
+	if err != nil {
+		return nil, errors.NewStackedError(err, "failed to read abi file")
+	}
+
+	parser, err := abi.JSON(strings.NewReader(string(file)))
+	if err != nil {
+		return nil, errors.NewStackedError(err, "failed to parse abi")
+	}
+
+	c := &ContractEventABI{
+		topicEventNames: make(map[string]string),
+	}
+
+	c.parser = parser
+
+	var topic string
+	for _, eventName := range eventNames {
+		event, ok := parser.Events[eventName]
+		if !ok {
+			return nil, fmt.Errorf("event name %v not found in ABI file %v", eventName, abiPath)
+		}
+		topic = event.Id().Hex()
+		c.topicEventNames[topic] = eventName
+	}
+
+	return c, nil
+}
