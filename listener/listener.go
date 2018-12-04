c664bd130932b035f5427ebf219e1fb8fb300ce2
wangyanyu <forjoin92@gmail.com>
2018-12-04 14:10:57 +0800

    add get contract events from receipt (#851)
    * add get contract events from receipt
    
    * Update listener.go
    
    * optimize code
    
    * fix typo error
    
    * check log.topics length

--- listener/listener.go
+++ listener/listener.go
@@ -11,12 +11,14 @@ import (
 	"strings"
 
 	"github.com/seeleteam/go-seele/accounts/abi"
+	"github.com/seeleteam/go-seele/common"
 	"github.com/seeleteam/go-seele/common/errors"
+	"github.com/seeleteam/go-seele/core/types"
 )
 
 var (
 	// ErrInvalidArguments is returned when NewContractEventABI arguments are invalid.
-	ErrInvalidArguments = errors.New("the abiPath and eventName should not be empty")
+	ErrInvalidArguments = errors.New("the abiPath, eventName and contract address cannot be empty")
 )
 
 // ContractEventABI represents contract event parser.
@@ -24,15 +26,27 @@ type ContractEventABI struct {
 	// the map key is topic, the value is event name,
 	// topic to compare with log's topic,
 	// event name to label the event how to deal.
-	topicEventNames map[string]string
+	topicEventNames map[common.Hash]string
+
+	// contract address, to avoid different contracts have the same event name and arguments
+	contract common.Address
 
 	parser abi.ABI
 }
 
 // NewContractEventABI returns a ContractEventABI instance.
-func NewContractEventABI(abiPath string, eventNames ...string) (*ContractEventABI, error) {
+func NewContractEventABI(abiPath string, contract common.Address, eventNames ...string) (*ContractEventABI, error) {
 	if len(abiPath) == 0 || len(eventNames) == 0 {
 		return nil, ErrInvalidArguments
+	}
+
+	if contract.Equal(common.EmptyAddress) {
+		return nil, ErrInvalidArguments
+	}
+
+	// ensure the contract address is EVM contract
+	if !contract.IsEVMContract() {
+		return nil, fmt.Errorf("the address is not EVM contract, %v", contract)
 	}
 
 	file, err := ioutil.ReadFile(abiPath)
@@ -46,20 +60,80 @@ func NewContractEventABI(abiPath string, eventNames ...string) (*ContractEventAB
 	}
 
 	c := &ContractEventABI{
-		topicEventNames: make(map[string]string),
+		contract:        contract,
+		topicEventNames: make(map[common.Hash]string),
 	}
 
 	c.parser = parser
 
-	var topic string
 	for _, eventName := range eventNames {
 		event, ok := parser.Events[eventName]
 		if !ok {
 			return nil, fmt.Errorf("event name %v not found in ABI file %v", eventName, abiPath)
 		}
-		topic = event.Id().Hex()
-		c.topicEventNames[topic] = eventName
+		c.topicEventNames[event.Id()] = eventName
 	}
 
 	return c, nil
+}
+
+// Event represents a contract event instance from Log.
+type Event struct {
+	Contract  common.Address
+	EventName string
+	Topic     common.Hash
+	Arguments []interface{}
+}
+
+// GetEvents get events from receipts.
+func (c *ContractEventABI) GetEvents(receipts []*types.Receipt) ([]*Event, error) {
+	var events []*Event
+	for _, receipt := range receipts {
+		eventsSlice, err := c.GetEvent(receipt)
+		if err != nil {
+			return nil, err
+		}
+
+		events = append(events, eventsSlice...)
+	}
+
+	return events, nil
+}
+
+// GetEvent get events from receipt.
+func (c *ContractEventABI) GetEvent(receipt *types.Receipt) ([]*Event, error) {
+	var events []*Event
+	if receipt.Failed {
+		return nil, nil
+	}
+
+	for _, log := range receipt.Logs {
+		if !log.Address.Equal(c.contract) || len(log.Topics) < 1 {
+			continue
+		}
+
+		eventName, ok := c.topicEventNames[log.Topics[0]]
+		if !ok {
+			continue
+		}
+
+		event := &Event{
+			Contract:  c.contract,
+			EventName: eventName,
+			Topic:     log.Topics[0],
+		}
+
+		var err error
+		if log.Data != nil {
+			// unnecessary to check whether parser.Events has the event name, we have check it before
+			event.Arguments, err = c.parser.Events[eventName].Inputs.UnpackValues(log.Data)
+			if err != nil {
+				return nil, fmt.Errorf("event name %v not found in ABI file", eventName)
+			}
+		}
+
+		events = append(events, event)
+	}
+
+	return events, nil
 }
