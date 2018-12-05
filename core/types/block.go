af190c6b69fe8f90898437a94d9cb81c271bfdde
ypenghui6 <ypenghui6@163.com>
2018-12-05 10:54:00 +0800

    fix comments

--- core/types/block.go
+++ core/types/block.go
@@ -52,7 +52,7 @@ type BlockHeader struct {
 	Difficulty        *big.Int       // Difficulty is the difficulty of the block
 	Height            uint64         // Height is the number of the block
 	CreateTimestamp   *big.Int       // CreateTimestamp is the timestamp when the block is created
-	Witness           []byte         //Witness is the block pow proof info or the sign of vote
+	Witness           []byte         // in pow consensus, witness is the nonce that proof whether the block is validate; in BFT consensus, witness is used to vote for validator candidates and Creator is the candidate address.
 	Consensus         ConsensusType
 	ExtraData         []byte // ExtraData stores the extra info of block header.
 }
