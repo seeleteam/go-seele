dce866e67a9c0e702b9e1b604ba90087e0c01c85
seele <zenglingkun168@gmail.com>
2019-07-18 09:25:45 -0700

    pbft consensus algorithm for subchain

---
+++ consensus/pbft/pbft.go
@@ -0,0 +1,8 @@
+package pbft
+
+type PBFT interface {
+	StartConsensus(request *RequestMsg) (*PrePrepareMsg, error)
+	PrePrepare(prePrepareMsg *PrePrepareMsg) (*VoteMsg, error)
+	Prepare(prepareMsg *VoteMsg) (*VoteMsg, error)
+	Commit(commitMsg *VoteMsg) (*ReplyMsg, *RequestMsg, error)
+}
