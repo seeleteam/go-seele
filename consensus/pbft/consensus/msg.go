e785697cf8dd8b9c162b529c0299ff82ef969343
seele <zenglingkun168@gmail.com>
2019-08-02 17:47:47 -0700

    optimization pbft algorithm

---
+++ consensus/pbft/consensus/msg.go
@@ -0,0 +1,37 @@
+package consensus
+
+type RequestMsg struct {
+	Timestamp  int64  `json:"timestamp"`
+	ClientID   string `json:"clientID"`
+	Operation  string `json:"operation"`
+	SequenceID int64  `json:"sequenceID"`
+}
+
+type ReplyMsg struct {
+	ViewID    int64  `json:"viewID"`
+	Timestamp int64  `json:"timestamp"`
+	ClientID  string `json:"clientID"`
+	NodeID    string `json:"nodeID"`
+	Result    string `json:"result"`
+}
+
+type PrePrepareMsg struct {
+	ViewID     int64       `json:"viewID"`
+	SequenceID int64       `json:"sequenceID"`
+	Digest     string      `json:"digest"`
+	RequestMsg *RequestMsg `json:"requestMsg"`
+}
+
+type VoteMsg struct {
+	ViewID     int64  `json:"viewID"`
+	SequenceID int64  `json:"sequenceID"`
+	Digest     string `json:"digest"`
+	NodeID     string `json:"nodeID"`
+	MsgType           `json:"msgType"`
+}
+
+type MsgType int
+const (
+	PrepareMsg MsgType = iota
+	CommitMsg
+)
