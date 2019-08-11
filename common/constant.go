521087c51cf88945750c0e1524320035fa61f080
zixing xin <zixingxin@zixings-MBP.attlocal.net>
2019-08-11 10:57:43 -0700

    change version number

--- common/constant.go
+++ common/constant.go
@@ -22,7 +22,7 @@ const (
 	SeeleVersion uint = 1
     
 	// SeeleVersion for simpler display
-	SeeleNodeVersion string = "v1.1.2"
+	SeeleNodeVersion string = "v1.1.3"
 
 	// ShardCount represents the total number of shards.
 	ShardCount = 4
