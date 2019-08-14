a35be4c7964002c7a764bcab89952fe037c6ebdf
Shawn <45467185+shawnxin89@users.noreply.github.com>
2019-08-14 09:57:51 -0700

    version 1.1.4; fix some issues related to block writing and syncing (#1000)
    * add another getMiningTarget function
    
    * fix a bug in getSecondMiningTarget function
    
    * add hash comparison to reduce forks
    
    * add a tool to check the consistency of the balances
    
    * fix a bug in cross-shard synchronization
    
    * fix bugs in cross-shard transactions
    
    * change the flag shortcut in checkbalance.go to resolve the conflicts
    
    * FOR TEST ONLY: remove debt validation
    
    * change checkbalance flags
    
    * add a size limit for debt manager pool; add a database to store debts in case the debt manager pool is full
    
    * add a new line to the end of core/pool_config.go
    
    * add a new line to the end of seele/debtManager.go
    
    * fix a typo in seeleservice.go
    
    * change the Seele reward parameter
    
    * modify reward condition
    
    * fix bugs in format printing
    
    * add new consensus engine
    
    * add spow api
    
    * modify cached_store_test.go
    
    * modify some unit tests because the block header structure has changed
    
    * fix a bug in engine.go
    
    * establish master account in the genesis block
    
    * add some test accounts
    
    * delete binary file
    
    * modify spow to use memory only
    
    * add dynamic adjustment for the size of hashpool
    
    * add bound check; modify initial balance of the master account; modify some unit tests
    
    * adjust the maximum difficulty
    
    * fix synchronization bugs
    
    * fix typo
    
    * create genesis timestamp;refine the genesis structure for future more shards
    
    * remove static node in node1.json
    
    * new master accounts
    
    * adjust unit test
    
    * add static nodes for mainnet connection
    
    * add static nodes for mainnet connection2
    
    * optimize object pool and peer synchronization
    
    * optimize doSelectNode to speed up the connection of all the shards
    
    * object_pool
    
    * disable print info
    
    * change the number of times of random node selection
    
    * update travis config
    
    * remove travis.yml
    
    * modify .travis.yml
    
    * optimize spow mining kernel
    
    * optimize spow engine
    
    * adjust the upper limit of number of bits
    
    * for test only
    
    * modify spow engine, remove mutex
    
    * enhance spow engine and forking logic; adjust difficulty for future blocks
    
    * add upper limit to bitsToNonceMap
    
    * add new logic to spow consensus engine; forcing disconnection of invalid peer
    
    * disable some backend messages
    
    * a small modification
    
    * change some infos to debug mode
    
    * add the rewriting of local fork
    
    * disable some warning messages
    
    * start using new algo on shard 1
    
    * for test only
    
    * initialize the blockchain at a given height
    
    * optimize lightchain synchronization; add lightchain rollback
    
    * disable a warning
    
    * update head block hash after reversing blockchain
    
    * disable a warning
    
    * fix a bug in chain reversal; add lightchain info
    
    * modify the unit tests
    
    * continue the program when the recovery point is not usable; fix a bug so that headblockhash and currentBlock are updated as expected
    
    * fix some unhandled errors
    
    * fix a format issue
    
    * fix a issus related to printing strings
    
    * fix a nil pointer issue
    
    * fix a bug related to transaction nonce
    
    * improve tx handling, hard fork at height 735000
    
    * adjust nonce handling
    
    * always chainhead while writing a block; improve the handling of txindices; disconnect peer if the ancestor is not found
    
    * change version number
    
    * recover height to block mapping
    
    * fix blockleaves && change the frequency of announce sending
    
    * disable some log info
    
    * change version to 1.1.4

--- common/constant.go
+++ common/constant.go
@@ -22,7 +22,7 @@ const (
 	SeeleVersion uint = 1
     
 	// SeeleVersion for simpler display
-	SeeleNodeVersion string = "v1.1.2"
+	SeeleNodeVersion string = "v1.1.4"
 
 	// ShardCount represents the total number of shards.
 	ShardCount = 4
