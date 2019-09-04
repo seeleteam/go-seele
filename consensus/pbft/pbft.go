ec6c98757091e5c173c53c0413a3c6e6d84f64b4
seele <zenglingkun168@gmail.com>
2019-09-04 16:37:50 -0700

    remove duplicate files

--- consensus/pbft/pbft.go
+++
@@ -1,8 +0,0 @@
-package pbft
-
-type PBFT interface {
-	StartConsensus(request *RequestMsg) (*PrePrepareMsg, error)
-	PrePrepare(prePrepareMsg *PrePrepareMsg) (*VoteMsg, error)
-	Prepare(prepareMsg *VoteMsg) (*VoteMsg, error)
-	Commit(commitMsg *VoteMsg) (*ReplyMsg, *RequestMsg, error)
-}
