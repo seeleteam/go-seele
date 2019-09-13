26a993de040d51797b7c1f339c2734990d89197a
seele <zenglingkun168@gmail.com>
2019-09-12 17:58:48 -0700

    handler for msg and event

---
+++ consensus/bft/core/p2p.go
@@ -0,0 +1,137 @@
+package core
+
+import (
+	"github.com/seeleteam/go-seele/common"
+	"github.com/seeleteam/go-seele/consensus/bft"
+)
+
+func (c *core) Start() error {
+	c.startNewRound(common.Big0)
+	c.subscribeEvents()
+	go c.handleEvents()
+	return nil
+}
+
+func (c *core) Stop() error {
+	c.stopTimer()
+}
+
+func (c *core) subscribeEvents() {
+	c.events = c.server.EventMux().Subscribe(
+		bft.RequestEvent{},
+		bft.MessageEvent{},
+		backlogEvent{},
+	)
+	c.timeoutSub = c.server.EventMux().Subscribe(
+		timeoutSub{},
+	)
+	c.finalCommittedSub = c.server.EventMux().Subscribe(
+		bft.FinalCommittedEvent{}, //TODO
+	)
+}
+func (c *core) unsubscribeEvents() {
+	c.events.Unsubscribe()
+	c.timeoutSub.Unsubscribe()
+	c.finalCommittedSub.Unsubscribe()
+}
+func (c *core) handleEvents() {
+	// reset
+	defer func() {
+		c.current = nil
+		c.handlerWg.Done()
+	}()
+
+	c.handlerWg.Add(1)
+	for {
+		select { // wait for channel and then execute
+		case event, ok := <-c.events.Chan():
+			if !ok {
+				return
+			}
+			switch e := event.Data.(type) {
+			case bft.RequestEvent:
+				req := &bft.Request{
+					Proposal: e.Proposal,
+				}
+				err := c.handleRequest(req)
+				if err == errFutureMessage {
+					c.storeRequestMsg(req)
+				}
+			case bft.MessageEvent:
+				if err := c.handleMsg(e.Payload); err == nil {
+					c.server.Gossip(c.verSet, e.Payload)
+				}
+			case backlogEvent: // internal event
+				if err := c.handleCheckedMsg(e.msg, e.src); err == nil {
+					p, err := e.msg.Payload()
+					if err != nil {
+						c.log.Warn("failed to get message payload with err", err)
+						continue
+					}
+					c.server.Gossip(c.verSet, p)
+				}
+			}
+		case _, ok := <-c.timeoutSub.Chan():
+			if !ok {
+				return
+			}
+			c.handleTimeoutMsg()
+		case e, ok := <-c.finalCommittedSub.Chan():
+			if !ok {
+				return
+			}
+			switch e.Data.(type) {
+			case bft.FinalCommittedEvent:
+				c.handleFinalCommitted()
+			}
+		}
+	}
+}
+func (c *core) sendEvent(event interface{}) {
+	c.server.EventMux().Post(event)
+}
+func (c *core) handleMsg(payload []byte) error {
+	msg := new(message)
+	if err := msg.ValidatePayload(payload, c.verifyFn); err != nil {
+		c.log.Error("failed to validate msg payload with err %s", err)
+		return err
+	}
+	_, src := c.verSet.GetByAddress(msg.Address)
+	if src == nil {
+		c.log.Error("invalid address in messageg %v", msg)
+		return ErrUnauthorizedAddress
+	}
+	return c.handleCheckedMsg(msg, src)
+}
+
+func (c *core) handleCheckedMsg(msg *message, src bft.Verifier) error {
+	// record the message if it is a future message
+	backlog := func(err error) error {
+		if err == errFutureMessage {
+			c.storeBacklog(msg, src)
+		}
+		return err
+	}
+
+	switch msg.Code {
+	case msgPreprepare:
+		return backlog(c.handlePreprepare(msg, src)) //TODO
+	case msgPrepare:
+		return backlog(c.handlePrepare(msg, src)) //TODO
+	case msgCommit:
+		return backlog(c.handleCommit(msg, src)) //TODO
+	case msgRoundChange:
+		return backlog(c.handleRounChange(msg, src)) //TODO
+	default:
+		c.log.Error("invalid message: msg %v address %s from %v", msg, c.address, src)
+	}
+	return errInvalidMessage
+}
+
+func (c *core) handleTimeoutMsg() {}
+
+func (c *core) handleFinalCommitted() error {
+	c.logger.Debug("Received a final committed proposal")
+	c.startNewRound(common.Big0)
+	return nil
+}
