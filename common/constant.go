c88a969b616b3a40b2cd8df0383c54cb174ec158
LincolnZeng <zenglingkun168@gmail.com>
2019-10-29 20:50:20 -0700

    change node version to 1.2.4

--- common/constant.go
+++ common/constant.go
@@ -22,7 +22,7 @@ const (
 	SeeleVersion uint = 1
 
 	// SeeleVersion for simpler display
-	SeeleNodeVersion string = "v1.2.3"
+	SeeleNodeVersion string = "v1.2.4"
 
 	// ShardCount represents the total number of shards.
 	ShardCount = 4
