6dad52c22fa45a989558d1e9b02089c6e7e3ab62
seele <zenglingkun168@gmail.com>
2019-11-15 23:07:12 -0800

    fix bug

--- node/node_rpc.go
+++ node/node_rpc.go
@@ -84,7 +84,9 @@ func (n *Node) startTCP(apis []rpc.API) error {
 				n.lock.RLock()
 				closed := n.tcpListener == nil
 				n.lock.RUnlock()
-				conn.Close() // Need to close this fault connect. Still have a chance to accept it later (tested)
+				if conn != nil {
+					conn.Close() // Need to close this fault connect. Still have a chance to accept it later (tested)
+				}
 				if closed {
 					return
 				}
