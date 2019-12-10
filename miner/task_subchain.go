eb02a07c1ba133d7c030d2be451d2a6e75533d44
seele <zenglingkun168@gmail.com>
2019-12-10 12:03:30 -0800

    abond subservice and subtask

---
+++ miner/task_subchain.go
@@ -0,0 +1,177 @@
+/**
+*  @file
+*  @copyright defined in go-seele/LICENSE
+ */
+
+package miner
+
+// import (
+// 	"math/big"
+// 	"time"
+
+// 	"github.com/seeleteam/go-seele/common"
+// 	"github.com/seeleteam/go-seele/common/memory"
+// 	"github.com/seeleteam/go-seele/consensus"
+// 	"github.com/seeleteam/go-seele/core"
+// 	"github.com/seeleteam/go-seele/core/state"
+// 	"github.com/seeleteam/go-seele/core/txs"
+// 	"github.com/seeleteam/go-seele/core/types"
+// 	"github.com/seeleteam/go-seele/database"
+// 	"github.com/seeleteam/go-seele/log"
+// )
+
+// // Task is a mining work for engine, containing block header, transactions, and transaction receipts.
+// type TaskSubChain struct {
+// 	header           *types.BlockHeader
+// 	txs              []*types.Transaction
+// 	receipts         []*types.Receipt
+// 	updatedVerifiers []common.Address
+// 	coinbase         common.Address
+// }
+
+// // NewTask return TaskSubChain object
+// func NewTaskSubChain(header *types.BlockHeader, coinbase common.Address, vers []common.Address) *TaskSubChain {
+// 	return &TaskSubChain{
+// 		header:           header,
+// 		coinbase:         coinbase,
+// 		updatedVerifiers: vers,
+// 	}
+// }
+
+// // applyTransactionsAndDebts TODO need to check more about the transactions, such as gas limit
+// func (task *TaskSubChain) applyTransactions(seele SeeleBackend, statedb *state.Statedb, accountStateDB database.Database, log *log.SeeleLog) error {
+// 	now := time.Now()
+// 	// entrance
+// 	memory.Print(log, "task applyTransactionsAndDebts entrance", now, false)
+
+// 	// choose transactions from the given txs
+// 	size := core.BlockByteLimit
+// 	// the reward tx will always be at the first of the block's transactions
+// 	reward, err := task.handleMinerRewardTx(statedb)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	task.chooseTransactions(seele, statedb, log, size)
+
+// 	log.Info("mining block height:%d, reward:%s, transaction number:%d, updated verifiers number: %d",
+// 		task.header.Height, reward, len(task.txs), len(task.updatedVerifiers))
+
+// 	batch := accountStateDB.NewBatch()
+// 	root, err := statedb.Commit(batch)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	task.header.StateHash = root
+
+// 	// exit
+// 	memory.Print(log, "task applyTransactionsAndDebts exit", now, true)
+
+// 	return nil
+// }
+
+// func (task *TaskSubChain) chooseDebts(seele SeeleBackend, statedb *state.Statedb, log *log.SeeleLog) int {
+// 	now := time.Now()
+// 	// entrance
+// 	memory.Print(log, "task chooseDebts entrance", now, false)
+
+// 	size := core.BlockByteLimit
+
+// 	for size > 0 {
+// 		debts, _ := seele.DebtPool().GetProcessableDebts(size)
+// 		if len(debts) == 0 {
+// 			break
+// 		}
+
+// 		for _, d := range debts {
+// 			err := seele.BlockChain().ApplyDebtWithoutVerify(statedb, d, task.coinbase)
+// 			if err != nil {
+// 				log.Warn("apply debt error %s", err)
+// 				seele.DebtPool().RemoveDebtByHash(d.Hash)
+// 				continue
+// 			}
+
+// 			size = size - d.Size()
+// 			task.debts = append(task.debts, d)
+// 		}
+// 	}
+
+// 	// exit
+// 	memory.Print(log, "task chooseDebts exit", now, true)
+
+// 	return size
+// }
+
+// // handleMinerRewardTx handles the miner reward transaction.
+// func (task *TaskSubChain) handleMinerRewardTx(statedb *state.Statedb) (*big.Int, error) {
+// 	reward := consensus.GetReward(task.header.Height)
+// 	rewardTx, err := txs.NewRewardTx(task.coinbase, reward, task.header.CreateTimestamp.Uint64())
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	rewardTxReceipt, err := txs.ApplyRewardTx(rewardTx, statedb)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	task.txs = append(task.txs, rewardTx)
+
+// 	// add the receipt of the reward tx
+// 	task.receipts = append(task.receipts, rewardTxReceipt)
+
+// 	return reward, nil
+// }
+
+// func (task *TaskSubChain) chooseTransactions(seele SeeleBackend, statedb *state.Statedb, log *log.SeeleLog, size int) {
+// 	now := time.Now()
+// 	// entrance
+// 	memory.Print(log, "task chooseTransactions entrance", now, false)
+
+// 	txIndex := 1 // the first tx is miner reward
+
+// 	for size > 0 {
+// 		txs, txsSize := seele.TxPool().GetProcessableTransactions(size)
+// 		if len(txs) == 0 {
+// 			break
+// 		}
+
+// 		for _, tx := range txs {
+// 			if err := tx.Validate(statedb, task.header.Height); err != nil {
+// 				seele.TxPool().RemoveTransaction(tx.Hash)
+// 				log.Error("failed to validate tx %s, for %s", tx.Hash.Hex(), err)
+// 				txsSize = txsSize - tx.Size()
+// 				continue
+// 			}
+
+// 			receipt, err := seele.BlockChain().ApplyTransaction(tx, txIndex, task.coinbase, statedb, task.header)
+// 			if err != nil {
+// 				seele.TxPool().RemoveTransaction(tx.Hash)
+// 				log.Error("failed to apply tx %s, %s", tx.Hash.Hex(), err)
+// 				txsSize = txsSize - tx.Size()
+// 				continue
+// 			}
+
+// 			task.txs = append(task.txs, tx)
+// 			task.receipts = append(task.receipts, receipt)
+// 			txIndex++
+// 		}
+
+// 		size -= txsSize
+// 	}
+
+// 	// exit
+// 	memory.Print(log, "task chooseTransactions exit", now, true)
+// }
+
+// // generateBlock builds a block from task
+// func (task *TaskSubChain) generateBlock() *types.Block {
+// 	return types.NewBlock(task.header, task.txs, task.receipts, task.debts)
+// }
+
+// // Result is the result mined by engine. It contains the raw task and mined block.
+// type Result struct {
+// 	task  *TaskSubChain
+// 	block *types.Block // mined block, with good nonce
+// }
--- seele/seeleservice_sub.go
+++ seele/seeleservice_sub.go
@@ -5,56 +5,56 @@
 
 package seele
 
-import (
-	"context"
-	"fmt"
+// import (
+// 	"context"
+// 	"fmt"
 
-	"github.com/seeleteam/go-seele/consensus"
-	"github.com/seeleteam/go-seele/database/leveldb"
-	"github.com/seeleteam/go-seele/log"
-	"github.com/seeleteam/go-seele/miner"
-	"github.com/seeleteam/go-seele/node"
-)
+// 	"github.com/seeleteam/go-seele/consensus"
+// 	"github.com/seeleteam/go-seele/database/leveldb"
+// 	"github.com/seeleteam/go-seele/log"
+// 	"github.com/seeleteam/go-seele/miner"
+// 	"github.com/seeleteam/go-seele/node"
+// )
 
-// NewSeeleServiceSubchain create SeeleService for subchain
-func NewSeeleServiceSubchain(ctx context.Context, conf *node.Config, log *log.SeeleLog, engine consensus.Engine) (sb *SeeleService, err error) {
-	sb = &SeeleService{
-		log:        log,
-		networkID:  conf.P2PConfig.NetworkID,
-		netVersion: conf.BasicConfig.Version,
-	}
+// // NewSeeleServiceSubchain create SeeleService for subchain
+// func NewSeeleServiceSubchain(ctx context.Context, conf *node.Config, log *log.SeeleLog, engine consensus.Engine) (sb *SeeleService, err error) {
+// 	sb = &SeeleService{
+// 		log:        log,
+// 		networkID:  conf.P2PConfig.NetworkID,
+// 		netVersion: conf.BasicConfig.Version,
+// 	}
 
-	serviceContext := ctx.Value("ServiceContext").(ServiceContext)
-	fmt.Printf("start to initiate service with conf %+v", conf)
+// 	serviceContext := ctx.Value("ServiceContext").(ServiceContext)
+// 	fmt.Printf("start to initiate service with conf %+v", conf)
 
-	// Initialize blockchain DB.
-	if err = sb.initBlockchainDB(&serviceContext); err != nil {
-		return nil, err
-	}
+// 	// Initialize blockchain DB.
+// 	if err = sb.initBlockchainDB(&serviceContext); err != nil {
+// 		return nil, err
+// 	}
 
-	leveldb.StartMetrics(sb.chainDB, "chaindb", log)
+// 	leveldb.StartMetrics(sb.chainDB, "chaindb", log)
 
-	// Initialize account state info DB.
-	if err = sb.initAccountStateDB(&serviceContext); err != nil {
-		return nil, err
-	}
+// 	// Initialize account state info DB.
+// 	if err = sb.initAccountStateDB(&serviceContext); err != nil {
+// 		return nil, err
+// 	}
 
-	sb.miner = miner.NewMinerSubchain(conf.SeeleConfig.Coinbase, sb, engine)
+// 	sb.miner = miner.NewMinerSubchain(conf.SeeleConfig.Coinbase, sb, engine)
 
-	// initialize and validate genesis
-	fmt.Printf("[subchain] newSeeleService engine %+v", engine)
-	if err = sb.initGenesisAndChain(&serviceContext, conf, -1); err != nil {
-		return nil, err
-	}
+// 	// initialize and validate genesis
+// 	fmt.Printf("[subchain] newSeeleService engine %+v", engine)
+// 	if err = sb.initGenesisAndChain(&serviceContext, conf, -1); err != nil {
+// 		return nil, err
+// 	}
 
-	if err = sb.initPool(conf); err != nil {
-		return nil, err
-	}
+// 	if err = sb.initPool(conf); err != nil {
+// 		return nil, err
+// 	}
 
-	if sb.seeleProtocol, err = NewSeeleProtocol(sb, log); err != nil {
-		sb.Stop()
-		log.Error("failed to create seeleProtocol in NewSeeleService, %s", err)
-		return nil, err
-	}
-	return sb, nil
-}
+// 	if sb.seeleProtocol, err = NewSeeleProtocol(sb, log); err != nil {
+// 		sb.Stop()
+// 		log.Error("failed to create seeleProtocol in NewSeeleService, %s", err)
+// 		return nil, err
+// 	}
+// 	return sb, nil
+// }
