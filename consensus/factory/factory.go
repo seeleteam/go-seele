c46dc1c5437a47cb3168b97bf087b85f5fefe152
seele <zenglingkun168@gmail.com>
2020-01-31 17:21:06 -0800

    add mpow target-set confidence and let miner choose the the one they want

--- consensus/factory/factory.go
+++ consensus/factory/factory.go
@@ -23,14 +23,14 @@ import (
 
 // GetConsensusEngine get consensus engine according to miner algorithm name
 // WARNING: engine may be a heavy instance. we should have as less as possible in our process.
-func GetConsensusEngine(minerAlgorithm string, folder string) (consensus.Engine, error) {
+func GetConsensusEngine(minerAlgorithm string, folder string, percentage int) (consensus.Engine, error) {
 	var minerEngine consensus.Engine
 	if minerAlgorithm == common.EthashAlgorithm {
 		minerEngine = ethash.New(ethash.GetDefaultConfig(), nil, false)
 	} else if minerAlgorithm == common.Sha256Algorithm {
 		minerEngine = pow.NewEngine(1)
 	} else if minerAlgorithm == common.SpowAlgorithm {
-		minerEngine = spow.NewSpowEngine(1, folder)
+		minerEngine = spow.NewSpowEngine(1, folder, percentage)
 	} else {
 		return nil, fmt.Errorf("unknown miner algorithm")
 	}
@@ -49,7 +49,7 @@ func GetBFTEngine(privateKey *ecdsa.PrivateKey, folder string) (consensus.Engine
 }
 
 func MustGetConsensusEngine(minerAlgorithm string) consensus.Engine {
-	engine, err := GetConsensusEngine(minerAlgorithm, "temp")
+	engine, err := GetConsensusEngine(minerAlgorithm, "temp", 10)
 	if err != nil {
 		panic(err)
 	}
