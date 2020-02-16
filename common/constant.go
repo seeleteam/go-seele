806233a2574b21914be6d0914369e86cb0586671
avadesian <chunxiangxu@gmail.com>
2020-02-16 13:14:08 +0800

    seele version 1.3.1 (#1100)

--- common/constant.go
+++ common/constant.go
@@ -22,7 +22,7 @@ const (
 	SeeleVersion uint = 1
 
 	// SeeleVersion for simpler display
-	SeeleNodeVersion string = "v1.3.0"
+	SeeleNodeVersion string = "v1.3.1"
 
 	// ShardCount represents the total number of shards.
 	ShardCount = 4
